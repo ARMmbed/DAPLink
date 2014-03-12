01dae591e6a6ae7fef5cbf4c59a174f1fb3a27cd
Donal Morrissey <donal.morrissey@gmail.com>
2014-03-12 15:04:40 +0000

    LPC11u35 interface: Updating USB PLL Clock Source to use System Oscillator.

--- shared/cmsis/TARGET_NXP/TARGET_LPC11UXX/system_LPC11Uxx.c
+++ shared/cmsis/TARGET_NXP/TARGET_LPC11UXX/system_LPC11Uxx.c
@@ -150,7 +150,7 @@
 #define MAINCLKSEL_Val        0x00000003              // Reset: 0x000
 #define SYSAHBCLKDIV_Val      0x00000001              // Reset: 0x001
 #define USBPLLCTRL_Val        0x00000023              // Reset: 0x000
-#define USBPLLCLKSEL_Val      0x00000000              // Reset: 0x000
+#define USBPLLCLKSEL_Val      0x00000001              // Reset: 0x000
 #define USBCLKSEL_Val         0x00000000              // Reset: 0x000
 #define USBCLKDIV_Val         0x00000001              // Reset: 0x001
 
