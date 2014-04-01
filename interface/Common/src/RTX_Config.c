8a847db7d65458156e8d8d4574d5c9d7126751b3
sg- <sam.w.grove@gmail.com>
2014-04-01 16:56:12 -0500

    Remove semihosting as being enabled by default. Update to mass erase for Kinetis devices. Single startup file for all K20DX128 builds. Project file changes to output dir and may break concatenate.py

--- interface/Common/src/RTX_Config.c
+++ interface/Common/src/RTX_Config.c
@@ -28,10 +28,10 @@
 //   <i> Define max. number of tasks that will run at the same time.
 //   <i> Default: 6
 #ifndef OS_TASKCNT
-    #if defined(NO_SEMIHOST)
-        #define OS_TASKCNT    12
-    #else
+    #ifdef SEMIHOST
         #define OS_TASKCNT    13
+    #else
+        #define OS_TASKCNT    12
     #endif
 #endif
 
--- interface/Common/src/main.c
+++ interface/Common/src/main.c
@@ -326,7 +326,7 @@ __task void main_task(void) {
 
 #ifndef BOARD_UBLOX_C027
     // Target running
-    target_set_state(RESET_RUN_WITH_DEBUG);
+    //target_set_state(RESET_RUN_WITH_DEBUG);
 #endif
 
     // start semihost task
