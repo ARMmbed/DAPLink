f3e5f3cce177cfe69fd1b0417227026c3d114004
Chris Reed <flit@me.com>
2014-04-07 16:33:13 -0500

    Cleaned up target_flash.inc files.
    - Also changed flash driver calls to pass 1 for erase size instead
      of the sector size, to ensure a single sector is erased.
    - Some minor project updates.
    
    Signed-off-by: Chris Reed <flit@me.com>

--- interface/flash_algo_mdk/MKXXX/flash_algo.c
+++ interface/flash_algo_mdk/MKXXX/flash_algo.c
@@ -110,10 +110,10 @@ int EraseChip (void)
  */
 int EraseSector (unsigned long adr)
 {
-    int status = flash_erase(&g_flash, adr, g_flash.PFlashSectorSize);
+    int status = flash_erase(&g_flash, adr, 1);
     if (status == kStatus_Success)
     {
-        status = flash_verify_erase(&g_flash, adr, g_flash.PFlashSectorSize, kFlashMargin_Normal);
+        status = flash_verify_erase(&g_flash, adr, 1, kFlashMargin_Normal);
     }
     flash_cache_clear();
     return (status != kStatus_Success);
