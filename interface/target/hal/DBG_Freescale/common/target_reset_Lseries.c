772dc17e9ed83b7b15623b1000798bb3c7302f8e
sg- <sam.w.grove@gmail.com>
2014-05-12 15:15:50 -0500

    Correct tabs to spaces

--- interface/target/hal/DBG_Freescale/common/target_reset_Lseries.c
+++ interface/target/hal/DBG_Freescale/common/target_reset_Lseries.c
@@ -16,11 +16,11 @@
 #include "target_reset.h"
 #include "swd_host.h"
 
-#define MDM_STATUS 	0x01000000
-#define MDM_CTRL 		0x01000004     //
-#define MDM_IDR 		0x010000fc     // read-only identification register
+#define MDM_STATUS  0x01000000
+#define MDM_CTRL    0x01000004     //
+#define MDM_IDR     0x010000fc     // read-only identification register
 
-#define MCU_ID     	0x001c0020
+#define MCU_ID      0x001c0020
 
 void target_before_init_debug(void) {
     swd_set_target_reset(1);
