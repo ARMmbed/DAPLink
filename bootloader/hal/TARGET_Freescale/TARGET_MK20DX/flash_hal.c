ARMmbed/DAPLink

commit f3e5f3cce177cfe69fd1b0417227026c3d114004

Cleaned up target_flash.inc files.
- Also changed flash driver calls to pass 1 for erase size instead
  of the sector size, to ensure a single sector is erased.
- Some minor project updates.

Signed-off-by: Chris Reed <flit@me.com>

bootloader/hal/TARGET_Freescale/TARGET_MK20DX/flash_hal.c
@@ -50,10 +50,10 @@ int flash_hal_erase_chip(void)
 
 int flash_hal_erase_sector(uint32_t adr)
 {
-    int status = flash_erase(&g_flash, adr, g_flash.PFlashSectorSize);
+    int status = flash_erase(&g_flash, adr, 1);
     if (status == kStatus_Success)
     {
-        status = flash_verify_erase(&g_flash, adr, g_flash.PFlashSectorSize, kFlashMargin_Normal);
+        status = flash_verify_erase(&g_flash, adr, 1, kFlashMargin_Normal);
     }
     flash_cache_clear();
     return (status != kStatus_Success);

interface/flash_algo_mdk/MKXXX/flash_algo.c
@@ -110,10 +110,10 @@ int EraseChip (void)
  */
 int EraseSector (unsigned long adr)
 {
-    int status = flash_erase(&g_flash, adr, g_flash.PFlashSectorSize);
+    int status = flash_erase(&g_flash, adr, 1);
     if (status == kStatus_Success)
     {
-        status = flash_verify_erase(&g_flash, adr, g_flash.PFlashSectorSize, kFlashMargin_Normal);
+        status = flash_verify_erase(&g_flash, adr, 1, kFlashMargin_Normal);
     }
     flash_cache_clear();
     return (status != kStatus_Success);

interface/flash_algo_mdk/MKXXX/MKXX.uvproj
@@ -355,7 +355,7 @@
             <uSurpInc>0</uSurpInc>
             <VariousControls>
               <MiscControls>--diag_suppress=1 --gnu</MiscControls>
-              <Define>CPU_MK20DX128VLH5</Define>
+              <Define>CPU_MK20DX128VLH5, __NO_EMBEDDED_ASM</Define>
               <Undefine></Undefine>
               <IncludePath>.;..\..\..\shared\cmsis\TARGET_Freescale\include;..\..\..\shared\cmsis;..\..\..\shared\flash_algo\TARGET_Freescale</IncludePath>
             </VariousControls>
@@ -861,8 +861,8 @@
             <uThumb>0</uThumb>
             <uSurpInc>0</uSurpInc>
             <VariousControls>
-              <MiscControls>--diag_suppress=1 --gnu</MiscControls>
-              <Define>CPU_MK64FN1M0VMD12</Define>
+              <MiscControls>--diag_suppress=1 --gnu --c99</MiscControls>
+              <Define>CPU_MK64FN1M0VMD12, __NO_EMBEDDED_ASM</Define>
               <Undefine></Undefine>
               <IncludePath>.;..\..\..\shared\cmsis\TARGET_Freescale\include;..\..\..\shared\cmsis;..\..\..\shared\flash_algo\TARGET_Freescale</IncludePath>
             </VariousControls>
@@ -1369,7 +1369,7 @@
             <uSurpInc>0</uSurpInc>
             <VariousControls>
               <MiscControls>--diag_suppress=1 --gnu</MiscControls>
-              <Define>CPU_MKL02Z32VFM4</Define>
+              <Define>CPU_MKL02Z32VFM4, __NO_EMBEDDED_ASM</Define>
               <Undefine></Undefine>
               <IncludePath>.;..\..\..\shared\cmsis\TARGET_Freescale\include;..\..\..\shared\cmsis;..\..\..\shared\flash_algo\TARGET_Freescale</IncludePath>
             </VariousControls>
@@ -1876,7 +1876,7 @@
             <uSurpInc>0</uSurpInc>
             <VariousControls>
               <MiscControls>--diag_suppress=1 --gnu</MiscControls>
-              <Define>CPU_MKL05Z32VLF4</Define>
+              <Define>CPU_MKL05Z32VLF4, __NO_EMBEDDED_ASM</Define>
               <Undefine></Undefine>
               <IncludePath>.;..\..\..\shared\cmsis\TARGET_Freescale\include;..\..\..\shared\cmsis;..\..\..\shared\flash_algo\TARGET_Freescale</IncludePath>
             </VariousControls>
@@ -2383,7 +2383,7 @@
             <uSurpInc>0</uSurpInc>
             <VariousControls>
               <MiscControls>--diag_suppress=1 --gnu</MiscControls>
-              <Define>CPU_MKL25Z128VLK4</Define>
+              <Define>CPU_MKL25Z128VLK4, __NO_EMBEDDED_ASM</Define>
               <Undefine></Undefine>
               <IncludePath>.;..\..\..\shared\cmsis\TARGET_Freescale\include;..\..\..\shared\cmsis;..\..\..\shared\flash_algo\TARGET_Freescale</IncludePath>
             </VariousControls>
@@ -2890,7 +2890,7 @@
             <uSurpInc>0</uSurpInc>
             <VariousControls>
               <MiscControls>--diag_suppress=1 --gnu</MiscControls>
-              <Define>CPU_MKL25Z128VLK4</Define>
+              <Define>CPU_MKL25Z128VLK4, __NO_EMBEDDED_ASM</Define>
               <Undefine></Undefine>
               <IncludePath>.;..\..\..\shared\cmsis\TARGET_Freescale\include;..\..\..\shared\cmsis;..\..\..\shared\flash_algo\TARGET_Freescale</IncludePath>
             </VariousControls>
@@ -3397,7 +3397,7 @@
             <uSurpInc>0</uSurpInc>
             <VariousControls>
               <MiscControls>--diag_suppress=1 --gnu</MiscControls>
-              <Define>CPU_MKL46Z256VMC4</Define>
+              <Define>CPU_MKL46Z256VMC4, __NO_EMBEDDED_ASM</Define>
               <Undefine></Undefine>
               <IncludePath>.;..\..\..\shared\cmsis\TARGET_Freescale\include;..\..\..\shared\cmsis;..\..\..\shared\flash_algo\TARGET_Freescale</IncludePath>
             </VariousControls>

interface/mdk/k20dx128/k20dx128_interface.uvproj
@@ -14756,8 +14756,8 @@
           </ArmAdsMisc>
           <Cads>
             <interw>1</interw>
-            <Optim>3</Optim>
-            <oTime>1</oTime>
+            <Optim>1</Optim>
+            <oTime>0</oTime>
             <SplitLS>0</SplitLS>
             <OneElfS>0</OneElfS>
             <Strict>0</Strict>

interface/target/hal/DBG_Freescale/DBG_K20D50M/target_flash.inc
@@ -57,14 +57,21 @@ static const TARGET_FLASH flash = {
     0x200000C5, // EraseSector
     0x200000F7, // ProgramPage
 
-    {0x20000001, 0x20000000+sizeof(K20D50M_FLM), 0x20000c00}, // {breakpoint, static_base, stack_pointer}
+    // breakpoint = RAM start + 1
+    // RSB : base address is address of Execution Region PrgData in map file
+    //       to access global/static data
+    // RSP : Initial stack pointer
+	{
+	    0x20000001, // breakpoint instruction address
+	    0x20000000 + 0x20 + 0x3b4,  // static base register value (image start + header + static base offset)
+	    0x20000c00  // initial stack pointer
+	},
 
-    0x20001c00, // program_buffer
-    0x20000000, // algo_start
-    sizeof(K20D50M_FLM), // algo_size
-    K20D50M_FLM,  // image
-
-    512,        // ram_to_flash_bytes_to_be_written
+	0x20001c00, // program_buffer, any valid RAM location with +512 bytes of headroom
+	0x20000000, // algo_start, start of RAM
+	sizeof(K20D50M_FLM), // algo_size, size of array above
+	K20D50M_FLM,  // image, flash algo instruction array
+    512        // ram_to_flash_bytes_to_be_written
 };
 
 

interface/target/hal/DBG_Freescale/DBG_K64F/target_flash.inc
@@ -58,19 +58,21 @@ static const TARGET_FLASH flash = {
     0x200000C5, // EraseSector
     0x200000F7, // ProgramPage
 
-    // RAM start + 1, RAM start + size of algo??, top of stack = start of RAM + 4096??
-	{0x20000001, 0x20000000+sizeof(K64F_FLM), 0x20001000}, // {breakpoint, static_base, stack_pointer}
+    // breakpoint = RAM start + 1
+    // RSB : base address is address of Execution Region PrgData in map file
+    //       to access global/static data
+    // RSP : Initial stack pointer
+	{
+	    0x20000001, // breakpoint instruction address
+	    0x20000000 + 0x20 + 0x3bc,  // static base register value (image start + header + static base offset)
+	    0x20001000  // initial stack pointer
+	},
 
-    // any valid RAM location with +512 bytes of headroom
-	0x20003000, // program_buffer
-    // start of RAM
-	0x20000000, // algo_start
-    // size of array above
-	sizeof(K64F_FLM), // algo_size
-    // flash algo instruction array
-	K64F_FLM,  // image
-
-    512,        // ram_to_flash_bytes_to_be_written
+	0x20003000, // program_buffer, any valid RAM location with +512 bytes of headroom
+	0x20000000, // algo_start, start of RAM
+	sizeof(K64F_FLM), // algo_size, size of array above
+	K64F_FLM,  // image, flash algo instruction array
+    512        // ram_to_flash_bytes_to_be_written
 };
 
 

interface/target/hal/DBG_Freescale/DBG_KL02Z/target_flash.inc
@@ -57,14 +57,21 @@ static const TARGET_FLASH flash = {
     0x200000B5, // EraseSector
     0x200000E7, // ProgramPage
 
-    {0x20000001, 0x20000000+sizeof(KL02Z_FLM), 0x20000800}, // {breakpoint, const data (flashprg), stack_pointer}
+    // breakpoint = RAM start + 1
+    // RSB : base address is address of Execution Region PrgData in map file
+    //       to access global/static data
+    // RSP : Initial stack pointer
+	{
+	    0x20000001, // breakpoint instruction address
+	    0x20000000 + 0x20 + 0x3a4,  // static base register value (image start + header + static base offset)
+	    0x20000800  // initial stack pointer
+	},
 
-    0x20000a00, // program_buffer, reserve 512 byte buffer for the flash to be written
-    0x20000000, // algo_start
-    sizeof(KL02Z_FLM), // algo_size
-    KL02Z_FLM,  // image
-
-    512,        // ram_to_flash_bytes_to_be_written
+	0x20000a00, // program_buffer, any valid RAM location with +512 bytes of headroom
+	0x20000000, // algo_start, start of RAM
+	sizeof(KL02Z_FLM), // algo_size, size of array above
+	KL02Z_FLM,  // image, flash algo instruction array
+    512        // ram_to_flash_bytes_to_be_written
 };
 
 

interface/target/hal/DBG_Freescale/DBG_KL05Z/target_flash.inc
@@ -57,14 +57,21 @@ static const TARGET_FLASH flash = {
     0x200000B5, // EraseSector
     0x200000E7, // ProgramPage
 
-    {0x20000001, 0x20000000+sizeof(KL05Z_FLM), 0x20000800}, // {breakpoint, const data (flashprg), stack_pointer}
+    // breakpoint = RAM start + 1
+    // RSB : base address is address of Execution Region PrgData in map file
+    //       to access global/static data
+    // RSP : Initial stack pointer
+	{
+	    0x20000001, // breakpoint instruction address
+	    0x20000000 + 0x20 + 0x3a4,  // static base register value (image start + header + static base offset)
+	    0x20000800  // initial stack pointer
+	},
 
-    0x20000a00, // program_buffer, reserve 512 byte buffer for the flash to be written
-    0x20000000, // algo_start
-    sizeof(KL05Z_FLM), // algo_size
-    KL05Z_FLM,  // image
-
-    512,        // ram_to_flash_bytes_to_be_written
+	0x20000a00, // program_buffer, any valid RAM location with +512 bytes of headroom
+	0x20000000, // algo_start, start of RAM
+	sizeof(KL05Z_FLM), // algo_size, size of array above
+	KL05Z_FLM,  // image, flash algo instruction array
+    512        // ram_to_flash_bytes_to_be_written
 };
 
 

commit 73bba493a9d51ec54cb17cff6f185f58b586011e

Fix Kinetis EraseSector function
The size to erase passed into flash_erase by EraseSector is 1 which
is invalid.  The function flash_check_range will return an error which
in turn will cause flash sector erase to fail.  This patch replaces
the fixed sector erase size of 1 with g_flash.PFlashSectorSize which
is a valid size.  This fixes interface updates from the bootloader.
This functionality was initially broken by the commit
f3e5f3cce177cfe69fd1b0417227026c3d114004 -
"Cleaned up target_flash.inc files."

bootloader/hal/TARGET_Freescale/TARGET_MK20DX/flash_hal.c
@@ -50,10 +50,10 @@ int flash_hal_erase_chip(void)
 
 int flash_hal_erase_sector(uint32_t adr)
 {
-    int status = flash_erase(&g_flash, adr, 1);
+    int status = flash_erase(&g_flash, adr, g_flash.PFlashSectorSize);
     if (status == kStatus_Success)
     {
-        status = flash_verify_erase(&g_flash, adr, 1, kFlashMargin_Normal);
+        status = flash_verify_erase(&g_flash, adr, g_flash.PFlashSectorSize, kFlashMargin_Normal);
     }
     flash_cache_clear();
     return (status != kStatus_Success);

interface/flash_algo_mdk/MKXXX/flash_algo.c
@@ -110,10 +110,10 @@ int EraseChip (void)
  */
 int EraseSector (unsigned long adr)
 {
-    int status = flash_erase(&g_flash, adr, 1);
+    int status = flash_erase(&g_flash, adr, g_flash.PFlashSectorSize);
     if (status == kStatus_Success)
     {
-        status = flash_verify_erase(&g_flash, adr, 1, kFlashMargin_Normal);
+        status = flash_verify_erase(&g_flash, adr, g_flash.PFlashSectorSize, kFlashMargin_Normal);
     }
     flash_cache_clear();
     return status;