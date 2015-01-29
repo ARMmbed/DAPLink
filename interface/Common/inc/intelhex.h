bed6dab4e81091c31b69fe45fc903518912d3eb3
sg- <sam.w.grove@gmail.com>
2015-01-29 00:42:55 -0600

    Moved hex parsing logic from test harness to interface app. Logic for target flash write need to be worked out from the test since not all data is present and the program call need to return.

--- interface/Common/inc/intelhex.h
+++ interface/Common/inc/intelhex.h
@@ -7,26 +7,36 @@
   extern "C" {
 #endif
       
+/** Type of states that the parser can return
+    @enum hex_parse_status_t
+ */
 typedef enum hex_parse_status_t hex_parse_status_t;
 enum hex_parse_status_t {
-    HEX_PARSE_OK = 0,
-    HEX_PARSE_EOF,
-    HEX_PARSE_UNALIGNED,
-    HEX_PARSE_LINE_OVERRUN
+    HEX_PARSE_OK = 0,       /*!< The input buffer was complete parsed and converted into the output buffer */
+    HEX_PARSE_EOF,          /*!< EOF line found in the hex file */
+    HEX_PARSE_UNALIGNED,    /*!< The address of decoded data isnt consecutive. Need to program what was returned and continue to parse the input buffer */
+    HEX_PARSE_LINE_OVERRUN, /*!< Error state when the record length is longer than the record structure */
+    HEX_PARSE_CKSUM_FAIL,   /*!< Error state when the record checksum doesnt properly compute */
+    HEX_PARSE_UNINIT        /*!< Default state. Return of this type is unrecoverable logic error */
 };
 
 /** Prepare any state that is maintained for the start of a file
- *   @return none
+    @param none
+    @return none
  */
 void reset_hex_parser(void);
 
 /** Convert a blob of hex data into its binary equivelant
- *   @param buf The input and formatted output buffer for data
- *   @param start_address The address the data converted in the buffer starts at
- *   @param size The amount of data in the buffer
- *   @return 1 if we have valid data to write in buf, 0 otherwise
+    @param hex_blob A block of ascii encoded hex file data
+    @param hex_blob_size The amount of valid data in the hex_blob
+    @param hex_parse_cnt The amount of hex_blob data from the call that was parsed
+    @param bin_buf Buffer the decoded hex file contents goes into
+    @param bin_buf_size max size of the buffer
+    @param bin_buf_address The start address for data in the bin_buf as decoded from the hex file
+    @param bin_buf_cnt The amount of data in the bin_buf
+    @return A member of hex_parse_status_t that describes the state of decoding
  */
-uint32_t parse_hex_blob(uint8_t *buf, uint32_t *start_address, uint32_t *size);
+hex_parse_status_t parse_hex_blob(uint8_t *hex_blob, uint32_t hex_blob_size, uint32_t *hex_parse_cnt, uint8_t *bin_buf, uint32_t bin_buf_size, uint32_t *bin_buf_address, uint32_t *bin_buf_cnt);
       
 #ifdef __cplusplus
   }
--- interface/target/hal/DBG_NordicSemi/DBG_nRF51822AA/target_flash.c
+++ interface/target/hal/DBG_NordicSemi/DBG_nRF51822AA/target_flash.c
@@ -23,13 +23,13 @@
 #include "target_flash.h"
 #include "target_config.h"
 #include "flash_blob.h"
+#include "DAP_config.h"
+#include "intelhex.h"
 
 #include "string.h"
 
-#define MIN_FLASH_ADDRESS   0x00000000
-#define MAX_FLASH_ADDRESS   0x00020000
-#define MIN_RAM_ADDRESS     0x20000000
-#define MAX_RAM_ADDRESS     0x20004000
+// maybe a parameter in write but this is just for now.
+extension_t file_extension;
 
 static /*inline*/ uint32_t test_range(const uint32_t test, const uint32_t min, const uint32_t max)
 {
@@ -45,34 +45,61 @@ uint8_t validate_bin_nvic(uint8_t *buf)
     uint32_t nvic_rv = 0;
     // test the initial SP value
     memcpy(&nvic_sp, buf, sizeof(nvic_sp));
-    if (0 == test_range(nvic_sp, MIN_RAM_ADDRESS, MAX_RAM_ADDRESS)) {
+    if (0 == test_range(nvic_sp, target_device.ram_start, target_device.ram_end)) {
         return 0;
     }
     // test the initial reset vector
     memcpy(&nvic_rv, buf+4, sizeof(nvic_rv));
-    if (0 == test_range(nvic_rv, MIN_FLASH_ADDRESS, MAX_FLASH_ADDRESS)) {
+    if (0 == test_range(nvic_rv, target_device.flash_start, target_device.flash_end)) {
         return 0;
     }
     
     return 1;
 }
 
+uint8_t validate_hexfile(uint8_t *buf)
+{
+    // look here for known hex records
+    // add hex identifier b[0] == ':' && b[8] == {'0', '2', '3', '4', '5'}
+    if (buf[0] == ':') {
+        if ((buf[8] == '0') ||
+            (buf[8] == '2') ||
+            (buf[8] == '3') ||
+            (buf[8] == '4') ||
+            (buf[8] == '5'))
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
 
-target_flash_status_t target_flash_init(void) {
+target_flash_status_t target_flash_init(extension_t ext)
+{
+    if (!target_set_state(RESET_PROGRAM)) {
+        return TARGET_FAIL_RESET;
+    }
+    
     // Download flash programming algorithm to target and initialise.
     if (!swd_write_memory(flash.algo_start, (uint8_t *)flash.image, flash.algo_size)) {
         return TARGET_FAIL_ALGO_DL;
     }
 
-    if (!swd_flash_syscall_exec(&flash.sys_call_param, flash.init, 0, 0 /* clk value is not used */, 0, 0)) {
+    if (!swd_flash_syscall_exec(&flash.sys_call_param, flash.init, target_device.flash_start, 0 /* clk value is not used */, 0, 0)) {
         return TARGET_FAIL_INIT;
     }
-
+    
+    file_extension = ext;
+    if (HEX == file_extension) {
+        reset_hex_parser();
+    }
+    
     return TARGET_OK;
 }
 
+
 target_flash_status_t target_flash_erase_sector(unsigned int sector) {
-    if (!swd_flash_syscall_exec(&flash.sys_call_param, flash.erase_sector, sector*FLASH_SECTOR_SIZE, 0, 0, 0)) {
+    if (!swd_flash_syscall_exec(&flash.sys_call_param, flash.erase_sector, sector*target_device.sector_size, 0, 0, 0)) {
         return TARGET_FAIL_ERASE_SECTOR;
     }
 
@@ -89,30 +116,88 @@ target_flash_status_t target_flash_erase_chip(void) {
     }
     
     target_set_state(RESET_PROGRAM);
-    target_flash_init();    
+    //target_flash_init();    
     
     return TARGET_OK;
 }
 
+static uint8_t bin_buffer[256] = {0};
+
 target_flash_status_t target_flash_program_page(uint32_t addr, uint8_t * buf, uint32_t size)
 {
-    uint32_t bytes_written = 0;
-	  // Program a page in target flash.
-    if (!swd_write_memory(flash.program_buffer, buf, size)) {
-        return TARGET_FAIL_ALGO_DATA_SEQ;
-    }
-    while(bytes_written < size) {
-        if (!swd_flash_syscall_exec(&flash.sys_call_param,
-                                    flash.program_page,
-                                    addr,                                     // arg1
-                                    flash.ram_to_flash_bytes_to_be_written,   // arg2
-                                    flash.program_buffer + bytes_written, 0)) { // arg3, arg4
-            return TARGET_FAIL_WRITE;
+    uint32_t bytes_written = 0, i = 0;
+    uint32_t bin_start_address = 0, bin_buf_written = 512, block_size = 512, block_amt_parsed = 0;
+    hex_parse_status_t status;
+    //ToDO - Fix this decode and program logic. May require some more helper methods
+    if (HEX == file_extension) {
+        do {
+            status = parse_hex_blob(buf, size, &block_amt_parsed, bin_buffer, sizeof(bin_buffer), &bin_start_address, &bin_buf_written);
+            if ((HEX_PARSE_EOF == status) || (HEX_PARSE_OK == status)) {
+                if ((block_size != block_amt_parsed) && (status != HEX_PARSE_EOF)) {
+                    // error state in parsing or record type
+                    block_amt_parsed = block_amt_parsed;
+                    //error("block parse amt failure\n");
+                }
+                //print the decoded file contents here
+                for (i = 0; i < bin_buf_written; i++) {
+                    //pc.putc(bin_buffer[i]);
+                }
+                block_size = 512;
+                buf += block_amt_parsed;
+            }
+            if (HEX_PARSE_UNALIGNED == status) {
+                //try to program the data here
+                for (i = 0; i < bin_buf_written; i++) {
+                    //pc.putc(bin_buffer[i]);
+                }
+                // pad the rest of the flash buffer with 0xff
+                for (i = bin_buf_written; i < 512; i++) {
+                    //pc.putc(0xff);
+                }
+                // incrememntal offset to finish the block
+                block_size = (512 - block_amt_parsed);
+                buf += block_amt_parsed;
+            }
+            if (HEX_PARSE_CKSUM_FAIL == status) {
+                // programming failure recorded to usere here
+                //error("cksum failure\n");
+            }
+            if (HEX_PARSE_UNINIT == status) {
+                //error("parser logic failure\n");
+            }
+            
+        } while(HEX_PARSE_EOF != status); // eject msc    
+    // this was the previous bin only logic. need to try and consolidate
+    } else {
+        target_flash_status_t status = TARGET_OK;
+        addr += target_device.flash_start;
+        
+        // we need to erase a sector
+        if (addr % target_device.sector_size == 0) {
+            status = target_flash_erase_sector(addr / target_device.sector_size);
+            if (status != TARGET_OK) {
+                return status;
+            }
         }
-        bytes_written += flash.ram_to_flash_bytes_to_be_written;
-        addr += flash.ram_to_flash_bytes_to_be_written;
+
+        // Program a page in target RAM to be programmed.
+        if (!swd_write_memory(flash.program_buffer, buf, size)) {
+            return TARGET_FAIL_ALGO_DATA_SEQ;
+        }
+
+        while(bytes_written < size) {
+            if (!swd_flash_syscall_exec(&flash.sys_call_param,
+                                        flash.program_page,
+                                        addr,
+                                        flash.ram_to_flash_bytes_to_be_written,
+                                        flash.program_buffer + bytes_written, 0)) {
+                return TARGET_FAIL_WRITE;
+            }
+
+            bytes_written += flash.ram_to_flash_bytes_to_be_written;
+            addr += flash.ram_to_flash_bytes_to_be_written;
+        }
     }
 
     return TARGET_OK;
 }
-
