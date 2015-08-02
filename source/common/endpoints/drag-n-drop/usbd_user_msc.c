9815e6f18b743f312e16deccb5e0c640ee442f0e
sg- <sam.w.grove@gmail.com>
2015-08-02 18:05:32 -0500

    adding comments about where to handle user IO events on the filesystem

--- source/common/endpoints/drag-n-drop/usbd_user_msc.c
+++ source/common/endpoints/drag-n-drop/usbd_user_msc.c
@@ -229,6 +229,9 @@ void usbd_msc_write_sect(uint32_t block, uint8_t *buf, uint32_t num_of_blocks)
             if (1 == exec_file_entry(tmp_file) && 0 != file_transfer_state.transfer_started) {
                 file_transfer_state.amt_to_write = tmp_file.filesize;
             } else if (1 == cfg_file_entry_mod(tmp_file)) {
+                // here we need a handler to modify the root directory entry and change the
+                //  target_device.cfg structure based on user input
+                debug_msg("%s", "USER CFG ACTION DETECTED\r\n");
                 goto cfg_action;
             }
         }
