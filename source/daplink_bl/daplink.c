f33719a8c847e713fd8caaab1e2d0c87fd5b6ad6
Russ Butler <russ.butler@arm.com>
2015-10-02 18:32:52 -0500

    Remove most conditional code based on DAPLINK_BL
    Remove the switch which differentiates the bootloader from
    interface.  Configuration differences between the bootloader
    and the interface should now be defined in either daplink_bl/daplink.c
    or daplink_if/daplink.c.

---
+++ source/daplink_bl/daplink.c
@@ -0,0 +1,34 @@
+/* CMSIS-DAP Interface Firmware
+ * Copyright (c) 2009-2013 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "daplink.h"
+#include "virtual_fs.h"
+
+// daplink_url_name and daplink_drive_name strings must 
+// be 11 characters excluding the null terminated character
+const char daplink_url_name[11] =   "HELP_FAQHTM";
+const char daplink_drive_name[11] = "MAINTENANCE";
+const char * const daplink_target_url = "https://mbed.com/daplink";
+
+bool daplink_is_bootloader()
+{
+    return true;
+}
+
+bool daplink_is_interface()
+{
+    return false;
+}
---
+++ source/daplink_if/daplink.c
@@ -0,0 +1,37 @@
+/* CMSIS-DAP Interface Firmware
+ * Copyright (c) 2009-2013 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "daplink.h"
+#include "virtual_fs.h"
+
+// daplink_url_name and daplink_drive_name strings must 
+// be 11 characters excluding the null terminated character
+__attribute__((weak))
+const char daplink_url_name[11] =   "MBED    HTM";
+__attribute__((weak))
+const char daplink_drive_name[11] = "DAPLINK    ";
+__attribute__((weak))
+const char * const daplink_target_url = "https://mbed.org/device/?code=@A";
+
+bool daplink_is_bootloader()
+{
+    return false;
+}
+
+bool daplink_is_interface()
+{
+    return true;
+}
