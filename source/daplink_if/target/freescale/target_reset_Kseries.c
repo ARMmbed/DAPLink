ARMmbed/DAPLink

commit ac113921e0e9af0c29b5f65bc7b336ea75b7d5ae

Add NXP device support (LPC1114)
* Add lpc11u35_lpc1114_if target in the projects.yaml
* Correct scatter file
* Add USBRAM and SRAM1 clock control
* Set proper stack size
* Avoid warnings in swd_host.c
* Enable sector erase rather than chip erase

source/daplink_if/target_flash.c
@@ -85,6 +85,7 @@ target_flash_status_t target_flash_erase_chip(void)
     return TARGET_OK;
 }
 
+#if 0
 static target_flash_status_t program_bin(uint32_t addr, uint8_t *buf, uint32_t size)
 {
     // called from msc logic so assumed that the smallest size is 512 (size of sector)
@@ -95,6 +96,7 @@ static target_flash_status_t program_bin(uint32_t addr, uint8_t *buf, uint32_t s
         if (1 == security_bits_set(addr, buf, size)) {
             return TARGET_FAIL_SECURITY_BITS;
         }
+
 //        // we need to erase a sector
 //        if (addr % target_device.sector_size == 0) {
 //            if (TARGET_OK != target_flash_erase_sector(addr / target_device.sector_size)) {
@@ -120,6 +122,59 @@ static target_flash_status_t program_bin(uint32_t addr, uint8_t *buf, uint32_t s
     }
     return TARGET_OK;
 }
+#else
+
+target_flash_status_t target_flash_erase_address(uint32_t address)
+{
+    if (0 == swd_flash_syscall_exec(&flash.sys_call_s, flash.erase_sector, address, 0, 0, 0)) {
+        return TARGET_FAIL_ERASE_SECTOR;
+    }
+    return TARGET_OK;
+}
+
+target_flash_status_t program_bin(uint32_t addr, uint8_t * buf, uint32_t size)
+{
+    uint32_t bytes_written = 0;
+    target_flash_status_t status = TARGET_OK;
+
+	
+    // we need to erase a sector
+    if (addr % target_device.sector_size == 0) {
+#if defined(BOARD_LPC4337) || defined(BOARD_LPC11U68)
+		// takes care about different sector size
+		if ((addr < SECTOR_BOUNDARY) || ((addr >= SECTOR_BOUNDARY) && (addr % SECTOR_BOUNDARY_ALIGN == 0))) {
+			status = target_flash_erase_address(addr);
+		}
+#else
+        status = target_flash_erase_address(addr);
+#endif
+		if (status != TARGET_OK) {
+            return status;
+        }
+    }
+
+    // Program a page in target flash.
+    if (!swd_write_memory(flash.program_buffer, buf, size)) {
+        return TARGET_FAIL_ALGO_DATA_SEQ;
+    }
+
+    while(bytes_written < size) {
+        if (!swd_flash_syscall_exec(&flash.sys_call_s,
+                                    flash.program_page,
+                                    addr,
+                                    flash.program_buffer_size,
+                                    flash.program_buffer + bytes_written,
+                                    0 )) {
+            return TARGET_FAIL_WRITE;
+        }
+
+        bytes_written += flash.program_buffer_size;
+        addr += flash.program_buffer_size;
+    }
+
+    return TARGET_OK;
+}
+#endif
 
 static const uint8_t ff_buffer[512] = {
     0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xff,

source/daplink_if/target/nxp/target_flash.c
@@ -81,6 +81,13 @@ target_flash_status_t target_flash_erase_sector(unsigned int sector)
     return TARGET_OK;
 }
 
+target_flash_status_t target_flash_erase_address(unsigned int addr)
+{
+    if (!swd_flash_syscall_exec(&flash.sys_call_param, flash.erase_sector, addr, 0, 0, 0)) {
+        return TARGET_FAIL_ERASE_SECTOR;
+    }
+    return TARGET_OK;
+}
 target_flash_status_t target_flash_erase_chip(void)
 {
     if (!swd_flash_syscall_exec(&flash.sys_call_param, flash.erase_chip, 0, 0, 0, 0)) {
@@ -96,7 +103,13 @@ target_flash_status_t target_flash_program_page(uint32_t addr, uint8_t * buf, ui
     target_flash_status_t status = TARGET_OK;
     // we need to erase a sector
     if (addr % target_device.sector_size == 0) {
-        status = target_flash_erase_sector(addr / target_device.sector_size);
+#if defined(BOARD_LPC4337) || defined(BOARD_LPC11U68)
+		if ((addr < SECTOR_BOUNDARY) || ((addr >= SECTOR_BOUNDARY) && (addr % SECTOR_BOUNDARY_ALIGN == 0))) {
+			status = target_flash_erase_address(addr);
+		}
+#else
+        status = target_flash_erase_address(addr);
+#endif
         if (status != TARGET_OK) {
             return status;
         }

projects.yaml
@@ -33,3 +33,8 @@ projects:
         - *module_if
         - records/common/rtos-cm0.yaml
         - records/projects/kl26z_nrf51822_if.yaml
+    lpc11u35_lpc1114_if:
+        - *module_tools
+        - *module_if
+        - records/common/rtos-cm0.yaml
+        - records/projects/lpc11u35_lpc1114_if.yaml

records/projects/lpc11u35_lpc1114_if.yaml
@@ -1,0 +1,28 @@
+common:
+    target:
+        - cortex-m0
+    macros:
+        - INTERFACE_LPC11U35
+        - TARGET_LPC1114
+        - APP_OFFSET_32K
+        - DAPLINK_IF
+        - OS_STKCHECK=0
+    includes:
+        - source/daplink_if/target/nxp/lpc1114
+    sources:
+        - source/daplink_if/target/nxp/lpc1114/lpc1114_app_config.c
+        - source/daplink_if/target/nxp/lpc1114/target_reset.c
+        - source/common/cmsis-core/nxp/lpc11u35
+        - source/common/cmsis-core/nxp/lpc11u35/armcc
+        - source/common/hal/nxp/lpc11u35
+
+tool_specific:
+    uvision:
+        linker_file:
+            - source/common/cmsis-core/nxp/lpc11u35/armcc/lpc11u35_daplink_if.sct
+        AfterMake:
+            RunUserProg1:
+                - $KARM\BIN\ElfDwT.exe #L1
+            RunUserProg2:
+                - "fromelf --bin $L@L.axf -o $L@L.bin"
+

source/common/cmsis-core/nxp/lpc11u35/armcc/lpc11u35_daplink_if.sct
@@ -1,20 +1,18 @@
 
-LR_IROM1 0x00005000 0xb000  {    ; load region size_region (64k)
+LR_IROM1 0x00000000 0x10000  {    ; load region size_region (64k)
 
-  ER_IROM1 0x00005000 0xb000  {  ; load address = execution address
-   *.o (RESET, +First)
-   *(InRoot$$Sections)
-   .ANY (+RO)
+  ER_IROM1 0x00000000 0x10000  {  ; load address = execution address   *.o (RESET, +First)
+    *.o (RESET, +First)
+    *(InRoot$$Sections)
+    .ANY (+RO)
   }
   
-  RW_IRAM1 0x100000c0 0x2000  {
-   .ANY (+RW +ZI)
-   system_LPC11Uxx.o (+RW +ZI)
-   startup_LPC11Uxx.o (+RW +ZI)
+  RW_IRAM1 0x10000000 0x2000  {
+    .ANY (+RW +ZI)
   }
   
   RW_IRAM2 0x20000000 0x800  {
-   .ANY (+RW +ZI)
+    .ANY (+RW +ZI)
   } 
   
 }

source/common/cmsis-core/nxp/lpc11u35/system_LPC11Uxx.c
@@ -386,7 +386,10 @@ void SystemCoreClockUpdate (void)            /* Get Core Clock Frequency      */
 void SystemInit (void) {
   volatile uint32_t i;
 
-	#if (CLOCK_SETUP)                                 /* Clock Setup              */
+  // Enable USBRAM and SRAM1
+  LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 26) | (1 << 27);
+
+#if (CLOCK_SETUP)                                 /* Clock Setup              */
 
 #if ((SYSPLLCLKSEL_Val & 0x03) == 1)
   LPC_SYSCON->PDRUNCFG     &= ~(1 << 5);          /* Power-up System Osc      */

source/common/hal/nxp/lpc11u35/IO_Config.h
@@ -14,21 +14,7 @@
  * limitations under the License.
  */
 
-#include "target_config.h"
+#ifndef __IO_CONFIG_H__
+#define __IO_CONFIG_H__
 
-// LPC1114 target information
-const target_cfg_t target_device = {
-    .board_id   = "1114",
-    .secret     = "78a0cd72",
-    .sector_size    = 4096,
-    // Assume memory is regions are same size. Flash algo should ignore requests
-    //  when variable sized sectors exist
-    // .sector_cnt = ((.flash_end - .flash_start) / .sector_size);
-    .sector_cnt     = (kB(32)/4096),
-    .flash_start    = 0,
-    .flash_end      = kB(32),
-    .ram_start      = 0x10000000,
-    .ram_end        = 0x10001000,
-    .disc_size      = kB(32)
-};
-
+#endif

source/common/hal/nxp/lpc11u35/flash_hal.c
@@ -1,146 +1,0 @@
-/* CMSIS-DAP Interface Firmware
- * Copyright (c) 2009-2013 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#include "flash_hal.h"        // FlashOS Structures
-
-#define SET_VALID_CODE 1       // Set Valid User Code Signature
-#define END_SECTOR     15
-/* Code Read Protection (CRP) */
-#define CRP_ADDRESS (0x000002FC)
-#define NO_ISP        (0x4E697370)
-#define CRP1        (0x12345678)
-#define CRP2        (0x87654321)
-#define CRP3        (0x43218765)
-#define IS_CRP_VALUE(v) ((v==NO_ISP) || (v==CRP1) || (v==CRP2) || (v==CRP3))
-#define NO_CRP      (1)     /* Forbid programming if CRP is enabled. */
-
-uint32_t _CCLK;           // CCLK in kHz
-
-struct sIAP {             // IAP Structure
-  uint32_t cmd;           // Command
-  uint32_t par[4];        // Parameters
-  uint32_t stat;          // Status
-  uint32_t res[2];        // Result
-} IAP;
-
-/* IAP Call */
-typedef void (*IAP_Entry) (uint32_t *cmd, uint32_t *stat);
-#define IAP_Call ((IAP_Entry) 0x1FFF1FF1)
-
-static uint32_t GetSecNum (uint32_t adr) {
-  uint32_t n;
-
-  n = adr >> 12;                               //  4kB Sector
-  if (n >= 0x10) {
-    n = 0x0E + (n >> 3);                       // 32kB Sector
-  }
-
-  return (n);                                  // Sector Number
-}
-
-int flash_hal_init (uint32_t clk) {
-  _CCLK = clk/1000;
-  return (0);
-}
-
-int flash_hal_uninit (uint32_t fnc) {
-  return (0);
-}
-
-int flash_hal_erase_chip (void) {
-  IAP.cmd    = 50;                             // Prepare Sector for Erase
-  IAP.par[0] = 0;                              // Start Sector
-  IAP.par[1] = END_SECTOR;                     // End Sector
-  IAP_Call (&IAP.cmd, &IAP.stat);              // Call IAP Command
-  if (IAP.stat) return (1);                    // Command Failed
-
-  IAP.cmd    = 52;                             // Erase Sector
-  IAP.par[0] = 0;                              // Start Sector
-  IAP.par[1] = END_SECTOR;                     // End Sector
-  IAP.par[2] = _CCLK;                          // CCLK in kHz
-  IAP_Call (&IAP.cmd, &IAP.stat);              // Call IAP Command
-  if (IAP.stat) return (1);                    // Command Failed
-
-  return (0);                                  // Finished without Errors
-}
-
-int flash_hal_erase_sector (uint32_t adr) {
-  uint32_t n = GetSecNum(adr);
-
-  IAP.cmd    = 50;                             // Prepare Sector for Erase
-  IAP.par[0] = n;                              // Start Sector
-  IAP.par[1] = n;                              // End Sector
-  IAP_Call (&IAP.cmd, &IAP.stat);              // Call IAP Command
-  if (IAP.stat)
-      return (1);                    // Command Failed
-
-  IAP.cmd    = 52;                             // Erase Sector
-  IAP.par[0] = n;                              // Start Sector
-  IAP.par[1] = n;                              // End Sector
-  IAP.par[2] = _CCLK;                          // CCLK in kHz
-  IAP_Call (&IAP.cmd, &IAP.stat);              // Call IAP Command
-  if (IAP.stat)
-      return (1);                    // Command Failed
-
-  return (0);                                  // Finished without Errors
-}
-
-int flash_hal_program_page (uint32_t adr, uint32_t sz, unsigned char *buf) {
-  uint32_t n;
-
-#if NO_CRP != 0
-  if (adr == 0) {
-      n = *((uint32_t *)(buf + CRP_ADDRESS));
-    if (IS_CRP_VALUE(n))
-    {
-        /* CRP is enabled, exit. */
-        return 1;
-    }
-  }
-#endif
-
-#if SET_VALID_CODE != 0                        // Set valid User Code Signature
-  if (adr == 0) {                              // Check for Vector Table
-    n = *((uint32_t *)(buf + 0x00)) +
-        *((uint32_t *)(buf + 0x04)) +
-        *((uint32_t *)(buf + 0x08)) +
-        *((uint32_t *)(buf + 0x0C)) +
-        *((uint32_t *)(buf + 0x10)) +
-        *((uint32_t *)(buf + 0x14)) +
-        *((uint32_t *)(buf + 0x18));
-    *((uint32_t *)(buf + 0x1C)) = 0 - n;  // Signature at Reserved Vector
-  }
-#endif
-
-  n = GetSecNum(adr);                          // Get Sector Number
-
-  IAP.cmd    = 50;                             // Prepare Sector for Write
-  IAP.par[0] = n;                              // Start Sector
-  IAP.par[1] = n;                              // End Sector
-  IAP_Call (&IAP.cmd, &IAP.stat);              // Call IAP Command
-  if (IAP.stat)
-      return (1);                    // Command Failed
-
-  IAP.cmd    = 51;                             // Copy RAM to Flash
-  IAP.par[0] = adr;                            // Destination Flash Address
-  IAP.par[1] = (uint32_t)buf;             // Source RAM Address
-  IAP.par[2] = 1024;                           // Fixed Page Size
-  IAP.par[3] = _CCLK;                          // CCLK in kHz
-  IAP_Call (&IAP.cmd, &IAP.stat);              // Call IAP Command
-  if (IAP.stat)
-      return (1);                    // Command Failed
-
-  return (0);                                  // Finished without Errors
-}

commit 2caf531860560eeff10cfb2a9011a65c55d8270b

Fix security bit check for hex and bin files
When programming a hex file the check for the security bits was
checking the incorrect address.  This causes hex programming to
fail on Kinetis targets.  This patch corrects this check and
allows any address to be passed into security_bits_set, rather
than exactly 0x400.

When programming a binary file the security bit check is missing.
This patch adds the security check back.  This bug was introduced
in the commit:
ac113921e0e9af0c29b5f65bc7b336ea75b7d5ae -
"Add NXP device support (LPC1114)"

source/daplink_if/target/freescale/target_reset_Kseries.c
@@ -116,21 +116,20 @@ uint8_t target_unlock_sequence(void) {
 // [0xf]=data flash protection bytes (FlexNVM devices only)
 //
 // This function checks that:
-// - 0x0-0xb==0xff
-// - 0xe-0xf==0xff
-// - FSEC=0xfe
+// - FSEC does not disable mass erase or secure the device.
 //
-// FOPT can be set to any value.
 uint8_t security_bits_set(uint32_t addr, uint8_t *data, uint32_t size)
 {
-    if (addr == 0x400) {
+    const uint32_t fsec_addr = 0x40C;
+    if ((addr <= fsec_addr) && (addr + size) > fsec_addr) {
+        uint8_t fsec = data[fsec_addr - addr];
         // make sure we can unsecure the device or dont program at all
-        if ((data[0xc] & 0x30) == 0x20) {
+        if ((fsec & 0x30) == 0x20) {
             // Dont allow programming mass-erase disabled state
             return 1;
         }
         // Security is OK long as we can mass-erase (comment the following out to enable target security)
-        if ((data[0xc] & 0x03) != 0x02) {
+        if ((fsec & 0x03) != 0x02) {
             return 1;
         }
     }
@@ -140,4 +139,3 @@ uint8_t security_bits_set(uint32_t addr, uint8_t *data, uint32_t size)
 uint8_t target_set_state(TARGET_RESET_STATE state) {
     return swd_set_target_state_hw(state);
 }
-

source/daplink_if/target/freescale/target_reset_Lseries.c
@@ -96,21 +96,20 @@ uint8_t target_unlock_sequence(void) {
 // [0xf]=data flash protection bytes (FlexNVM devices only)
 //
 // This function checks that:
-// - 0x0-0xb==0xff
-// - 0xe-0xf==0xff
-// - FSEC=0xfe
+// - FSEC does not disable mass erase or secure the device.
 //
-// FOPT can be set to any value.
 uint8_t security_bits_set(uint32_t addr, uint8_t *data, uint32_t size)
 {
-    if (addr == 0x400) {
+    const uint32_t fsec_addr = 0x40C;
+    if ((addr <= fsec_addr) && (addr + size) > fsec_addr) {
+        uint8_t fsec = data[fsec_addr - addr];
         // make sure we can unsecure the device or dont program at all
-        if ((data[0xc] & 0x30) == 0x20) {
+        if ((fsec & 0x30) == 0x20) {
             // Dont allow programming mass-erase disabled state
             return 1;
         }
         // Security is OK long as we can mass-erase (comment the following out to enable target security)
-        if ((data[0xc] & 0x03) != 0x02) {
+        if ((fsec & 0x03) != 0x02) {
             return 1;
         }
     }
@@ -120,4 +119,3 @@ uint8_t security_bits_set(uint32_t addr, uint8_t *data, uint32_t size)
 uint8_t target_set_state(TARGET_RESET_STATE state) {
     return swd_set_target_state(state);
 }
-

source/daplink_if/target_flash.c
@@ -145,6 +145,10 @@ target_flash_status_t program_bin(uint32_t addr, uint8_t * buf, uint32_t size)
     uint32_t bytes_written = 0;
     target_flash_status_t status = TARGET_OK;
 
+    // check if security bits were set
+    if (1 == security_bits_set(addr, buf, size)) {
+        return TARGET_FAIL_SECURITY_BITS;
+    }
 	
     // we need to erase a sector
     if (addr % target_device.sector_size == 0) {
@@ -218,8 +222,8 @@ static target_flash_status_t flexible_program_block(uint32_t addr, uint8_t *buf,
 
     // store the block start address if aligned with the programming size
     uint32_t target_flash_address = (addr / flash->program_buffer_size) * flash->program_buffer_size;
-    // check if security bits were set. Could be an odd alignment boundry that breaks (bin_buf only has part of security region)
-    if (1 == security_bits_set(target_flash_address, buf, size)) {
+    // check if security bits were set
+    if (1 == security_bits_set(addr, buf, size)) {
         return TARGET_FAIL_SECURITY_BITS;
     }
 //    // possibly erase a sector