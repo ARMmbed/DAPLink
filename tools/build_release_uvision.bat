c7f9336a53ec48e2018ed6d2891d33cd2e869b2d
John Cortell <john.cortell@nxp.com>
2016-03-29 12:58:02 -0500

    uVision pre/post script failure did not fail build

--- tools/build_release_uvision.bat
+++ tools/build_release_uvision.bat
@@ -16,21 +16,32 @@
 :: limitations under the License.
 ::
 rmdir /q /s uvision_release
+@if %errorlevel% neq 0 exit /B %errorlevel%
+
 mkdir uvision_release
+@if %errorlevel% neq 0 exit /B %errorlevel%
+
+@git --version 2> nul
+@if %errorlevel% neq 0 echo Error: git must be in PATH && exit /B %errorlevel%
+
 git rev-parse --verify HEAD                     > uvision_release\git_info.txt
 git diff --no-ext-diff --quiet --exit-code      >> uvision_release\git_info.txt
+
 echo Uncommitted Changes: %errorlevel%          >> uvision_release\git_info.txt
+
 virtualenv env
 call env\Scripts\activate
-REM use project requirements if not specified
+
+@REM use project requirements if not specified
 if [%1]==[] pip install -r requirements.txt
-REM use custom requirements if specified
+@REM use custom requirements if specified
 if not [%1]==[] pip install -r %1
 pip freeze                                      > uvision_release\build_requirements.txt
-REM build but continue if there are errors
-progen generate -t uvision -b
-SET LEVEL=%ERRORLEVEL%
-python tools/copy_release_files.py
-if %errorlevel% neq 0 exit %errorlevel%
-exit /B %level%
 
+@REM build but continue if there are errors
+progen generate -t uvision -b
+@SET level=%errorlevel%
+python tools/copy_release_files.py
+@if %errorlevel% neq 0 exit /B %errorlevel%
+
+@exit /B %level%
