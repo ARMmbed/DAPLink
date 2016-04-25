1a072522fc0e096520084fcd7687102225719f6c
jixing.zou <jixing.zou@redbear.hk>
2016-04-25 10:35:59 +0800

    stm32f103rb : change STM32F103RB -> STM32F103XB

--- source/daplink/RTX_Config.c
+++ source/daplink/RTX_Config.c
@@ -91,7 +91,7 @@
 #define OS_CLOCK    96000000
 #elif defined(INTERFACE_LPC4322)
 #define OS_CLOCK    204000000
-#elif defined(INTERFACE_STM32F103RB)
+#elif defined(INTERFACE_STM32F103XB)
 #define OS_CLOCK    72000000
 #endif
 #endif
