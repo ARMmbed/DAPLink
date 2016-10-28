c074a58052873296fa9f918341cb0e06e6ab0c03
Gaute Gamnes <gaute.gamnes@nordicsemi.no>
2016-10-28 07:53:39 +0200

    Added swd_uninit_debug() function. Used new function in reset routine for nRF5x, as well as target_flash.c uninit.

--- source/daplink/interface/swd_host.c
+++ source/daplink/interface/swd_host.c
@@ -830,6 +830,25 @@ uint8_t swd_init_debug(void)
     return 1;
 }
 
+uint8_t swd_uninit_debug(void)
+{
+    // Assume debug is powered up already
+    // Clear (CSYSPWRUPREQ | CDBGPWRUPREQ) in DP_CTRL_STAT register
+    uint32_t ctrl_stat_read;
+    
+    if (!swd_write_dp(DP_CTRL_STAT, 0x0)) {
+        return 0;
+    }
+    
+    do {
+        if (!swd_read_dp(DP_CTRL_STAT, &ctrl_stat_read)) {
+            return 0;
+        }
+    } while ((ctrl_stat_read & (CSYSPWRUPREQ | CDBGPWRUPREQ)));
+    
+    return 1;
+}
+
 __attribute__((weak)) void swd_set_target_reset(uint8_t asserted)
 {
     (asserted) ? PIN_nRESET_OUT(0) : PIN_nRESET_OUT(1);
