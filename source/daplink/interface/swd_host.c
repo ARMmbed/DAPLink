ARMmbed/DAPLink

commit c074a58052873296fa9f918341cb0e06e6ab0c03

Added swd_uninit_debug() function. Used new function in reset routine for nRF5x, as well as target_flash.c uninit.

source/daplink/interface/swd_host.c
@@ -830,6 +830,25 @@ uint8_t swd_init_debug(void)
     return 1;
 }
 
+uint8_t swd_uninit_debug(void)
+{
+    // Assume debug is powered up already
+    // Clear (CSYSPWRUPREQ | CDBGPWRUPREQ) in DP_CTRL_STAT register
+    uint32_t ctrl_stat_read;
+    
+    if (!swd_write_dp(DP_CTRL_STAT, 0x0)) {
+        return 0;
+    }
+    
+    do {
+        if (!swd_read_dp(DP_CTRL_STAT, &ctrl_stat_read)) {
+            return 0;
+        }
+    } while ((ctrl_stat_read & (CSYSPWRUPREQ | CDBGPWRUPREQ)));
+    
+    return 1;
+}
+
 __attribute__((weak)) void swd_set_target_reset(uint8_t asserted)
 {
     (asserted) ? PIN_nRESET_OUT(0) : PIN_nRESET_OUT(1);

source/daplink/interface/target_flash.c
@@ -79,6 +79,7 @@ static error_t target_flash_uninit(void)
         target_set_state(RESET_RUN);
     }
 
+    swd_uninit_debug();
     swd_off();
     return ERROR_SUCCESS;
 }

source/target/nordic/target_reset_nrf5x_sam3u2c.c
@@ -70,8 +70,11 @@ void swd_set_target_reset(uint8_t asserted)
                 swd_write_memory(0xE000ED0C, (uint8_t *)&swd_mem_write_data, 4);
                 //os_dly_wait(1);
             }
+            swd_uninit_debug();
         }
-        else {           
+        else {
+            swd_init_debug();
+            
             swd_read_ap(0x010000FC, &ap_index_return);
             if (ap_index_return == 0x02880000) {
                 // Device has CTRL-AP
@@ -80,7 +83,7 @@ void swd_set_target_reset(uint8_t asserted)
             else {
                 // No CTRL-AP - Soft reset has been performed
             }
-            PIOA->PIO_MDER = PIN_SWDIO | PIN_SWCLK | PIN_nRESET;
+            swd_uninit_debug();
         }
     }
     else {

source/daplink/interface/swd_host.h
@@ -33,6 +33,7 @@ extern "C" {
 uint8_t swd_init(void);
 uint8_t swd_off(void);
 uint8_t swd_init_debug(void);
+uint8_t swd_uninit_debug(void);
 uint8_t swd_read_dp(uint8_t adr, uint32_t *val);
 uint8_t swd_write_dp(uint8_t adr, uint32_t val);
 uint8_t swd_read_ap(uint32_t adr, uint32_t *val);

commit 7d9131865f88f8c02334460e1e55693225394bc1

Fix target reset problems on lpc11u35 interface
On some boards reset doesn't work correctly when the debug module is
turned off while the target is halted. This patch fixes that problem by
always turning on debug in swd_set_target_state_*.

This problem can be reproduced by programming the target on a
LPC812 Xpresso. The target will not start running when the reset
button is pressed. The board needs to be disconnected and
reconnected for the target to run.

This problem was introduced in the commit
c074a58052873296fa9f918341cb0e06e6ab0c03 -
Added swd_uninit_debug() function. Used new function in reset routine
for nRF5x, as well as target_flash.c uninit.

source/daplink/interface/swd_host.c
@@ -859,10 +859,13 @@ uint8_t swd_set_target_state_hw(TARGET_RESET_STATE state)
 {
     uint32_t val;
     swd_init();
+    swd_init_debug();
 
     switch (state) {
         case RESET_HOLD:
             swd_set_target_reset(1);
+            swd_uninit_debug();
+            swd_off();
             break;
 
         case RESET_RUN:
@@ -870,6 +873,7 @@ uint8_t swd_set_target_state_hw(TARGET_RESET_STATE state)
             os_dly_wait(2);
             swd_set_target_reset(0);
             os_dly_wait(2);
+            swd_uninit_debug();
             swd_off();
             break;
 
@@ -956,10 +960,13 @@ uint8_t swd_set_target_state_sw(TARGET_RESET_STATE state)
 {
     uint32_t val;
     swd_init();
+    swd_init_debug();
 
     switch (state) {
         case RESET_HOLD:
             swd_set_target_reset(1);
+            swd_uninit_debug();
+            swd_off();
             break;
 
         case RESET_RUN:
@@ -967,6 +974,7 @@ uint8_t swd_set_target_state_sw(TARGET_RESET_STATE state)
             os_dly_wait(2);
             swd_set_target_reset(0);
             os_dly_wait(2);
+            swd_uninit_debug();
             swd_off();
             break;
 

source/daplink/interface/target_flash.c
@@ -77,10 +77,10 @@ static error_t target_flash_uninit(void)
     // Resume the target if configured to do so
     if (config_get_auto_rst()) {
         target_set_state(RESET_RUN);
+    } else {
+        target_set_state(RESET_HOLD);
     }
 
-    swd_uninit_debug();
-    swd_off();
     return ERROR_SUCCESS;
 }
 

source/target/nordic/target_reset.c
@@ -46,8 +46,6 @@ uint8_t target_set_state(TARGET_RESET_STATE state)
 void swd_set_target_reset(uint8_t asserted)
 {
     if (asserted) {
-        swd_init_debug();
-
         //Set POWER->RESET on NRF to 1
         if (!swd_write_ap(AP_TAR, 0x40000000 + 0x544)) {
             return;

source/target/nordic/target_reset_nrf5x_sam3u2c.c
@@ -57,8 +57,6 @@ void swd_set_target_reset(uint8_t asserted)
                                 && board_id[3] == '1') ? 1 : 0;  // ID 1101 is the nrf52-dk
     if (nrf52_dk_is_used) {
         if (asserted) {
-            swd_init_debug();
-            
             swd_read_ap(0x010000FC, &ap_index_return);
             if (ap_index_return == 0x02880000) {
                 // Device has CTRL-AP
@@ -70,11 +68,8 @@ void swd_set_target_reset(uint8_t asserted)
                 swd_write_memory(0xE000ED0C, (uint8_t *)&swd_mem_write_data, 4);
                 //os_dly_wait(1);
             }
-            swd_uninit_debug();
         }
         else {
-            swd_init_debug();
-            
             swd_read_ap(0x010000FC, &ap_index_return);
             if (ap_index_return == 0x02880000) {
                 // Device has CTRL-AP
@@ -83,13 +78,10 @@ void swd_set_target_reset(uint8_t asserted)
             else {
                 // No CTRL-AP - Soft reset has been performed
             }
-            swd_uninit_debug();
         }
     }
     else {
         if (asserted) {
-            swd_init_debug();
-            
             /*  There is no reset pin on the nRF51822, so we need to use special reset routine, 
                 SWDCLK and SWDIO/nRESET needs to be kept low for a minimum of 100uS. Called of this func needs to make sure of this. */
             //Set POWER->RESET on NRF to 1