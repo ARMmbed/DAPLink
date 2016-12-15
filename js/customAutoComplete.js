48dfde25021f84547e373984f00822fce7971465
Austin Blackstone <austinblackstoneengineering@gmail.com>
2016-12-14 21:33:14 -0600

    added all supported boards to autocomplete list. Skeleton for instructions in place

--- js/customAutoComplete.js
+++ js/customAutoComplete.js
@@ -1,5 +1,44 @@
+---
+---
 var options = {
-	data: ["blue", "green", "pink", "red", "yellow"]
+	data:[
+		{% for thing in site.data.update %} 
+		{
+			"name": "{{thing.name}}", 
+			"code": "{{thing.product_code}}",
+			"logoURL": "{{thing.logoURL}}",
+			"instructions": "{{thing.instructions}}"
+		},
+  		{% endfor %}
+ ],
+
+	getValue: "name",
+
+	list: {
+		match: {
+			enabled: true
+		},
+		sort:{
+			enabled:true
+		},
+		maxNumberOfElements: 8,
+
+		showAnimation: {
+			type: "slide",
+			time: 300
+		},
+		hideAnimation: {
+			type: "slide",
+			time: 300
+		},
+		onChooseEvent: function(){
+			var value = $("#update-search").getSelectedItemData().instructions;
+			$("#update-instructions").html(value)
+		}
+	},
+
+	theme: "round"
+
 };
 
 $("#update-search").easyAutocomplete(options);
