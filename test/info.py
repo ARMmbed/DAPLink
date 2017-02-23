d6140ba992d66c3588a00dfab871c10927940571
modtronix-com <git1@modtronix.com>
2017-02-24 09:00:41 +1100

    Added board to SUPPORTED_CONFIGURATIONS and PROJECT_RELEASE_INFO in info.py

--- test/info.py
+++ test/info.py
@@ -67,6 +67,7 @@ PROJECT_RELEASE_INFO = {
     ('lpc11u35_6lowpan_borderrouterusb_if',         None,       0x0000,     "bin"       ),
     ('lpc11u35_6lowpan_borderrouterethernet_if',    None,       0x0000,     "bin"       ),
     ('lpc11u35_gr_peach_if',                        None,       0x0000,     "bin"       ),
+    ('lpc11u35_nz32_sc151_if',                      False,      0x0000,     "bin"       ),
 }
 
 # All supported configurations
@@ -125,6 +126,7 @@ SUPPORTED_CONFIGURATIONS = [
     (   0x7404,     'lpc11u35_6lowpan_borderrouterethernet_if', None,               'mbed-6LoWPAN-Border-Router-ETHERNET'   ),
     (   0xC000,     'lpc11u35_cocorico_if',                     None,               None                                    ), # TODO - set target to 'CoCo-ri-Co' when mbed-os supports this
     (   0x5500,     'lpc11u35_gr_peach_if',                     None,               'Renesas-GR-PEACH'                      ),
+    (   0x6660,     'lpc11u35_nz32_sc151_if',                   None,               'NZ32-SC151'                            ),
 ]
 
 # Add new HICs here
