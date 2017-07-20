3233822a2680f0097983c5da38d278e27d679299
Tony Wu <tonywu@realtek.com>
2017-07-20 19:15:33 +0800

    daplink - export swd read/write word api
    Signed-off-by: Tony Wu <tonywu@realtek.com>

--- source/daplink/interface/swd_host.c
+++ source/daplink/interface/swd_host.c
@@ -392,7 +392,7 @@ static uint8_t swd_write_data(uint32_t address, uint32_t data)
 }
 
 // Read 32-bit word from target memory.
-static uint8_t swd_read_word(uint32_t addr, uint32_t *val)
+uint8_t swd_read_word(uint32_t addr, uint32_t *val)
 {
     if (!swd_write_ap(AP_CSW, CSW_VALUE | CSW_SIZE32)) {
         return 0;
@@ -406,7 +406,7 @@ static uint8_t swd_read_word(uint32_t addr, uint32_t *val)
 }
 
 // Write 32-bit word to target memory.
-static uint8_t swd_write_word(uint32_t addr, uint32_t val)
+uint8_t swd_write_word(uint32_t addr, uint32_t val)
 {
     if (!swd_write_ap(AP_CSW, CSW_VALUE | CSW_SIZE32)) {
         return 0;
