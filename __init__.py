a7497dd710592f6ad34f8b9c2496d89f4caec77f
Sam Grove <sam.w.grove@gmail.com>
2017-11-08 15:40:02 -0600

    Update user instructions and board names
    Modification of instructions for LPC11U35 based boards and replace
    name with slug to match the platform URL with query name.

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ x = requests.get('https://os.mbed.com/api/v3/platforms/')
 for thing in x.json():
     for otherthing in thing['features']:
         if otherthing['name'] == 'DAPLink':
-            print'- %s:\n  name: %s\n  product_code: %s\n  fw_name: \n  instructions: \n    windows: \n    osx: \n    linux: \n' % (thing['name'], thing['name'], thing['productcode'])
+            print'- %s:\n  name: %s\n  product_code: %s\n  fw_name: \n  instructions: \n    windows: \n    osx: \n    linux: \n' % (thing['slug'], thing['slug'], thing['productcode'])
