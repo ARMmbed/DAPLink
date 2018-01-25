106dc26a90d4486b3a0d70fbb49544c7b1040654
sarahmarshy <sarah.marsh.engineer@gmail.com>
2018-01-25 17:55:33 -0600

    Enable K64F sector erase and lists target sector information in flash_blob.c (#374)
    * Set up architecture to store sector information in daplink
    
    enable k64f sector erase
    use sector information to enable alignment when erasing sectors
    
    * Add K64F sector information
    
    * Use old flash algo

--- source/target/freescale/k64f/target.c
+++ source/target/freescale/k64f/target.c
@@ -3,7 +3,7 @@
  * @brief   Target information for the k64f
  *
  * DAPLink Interface Firmware
- * Copyright (c) 2009-2016, ARM Limited, All Rights Reserved
+ * Copyright (c) 2017-2017, ARM Limited, All Rights Reserved
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -26,11 +26,11 @@
 
 // target information
 target_cfg_t target_device = {
-    .sector_size    = 4096,
-    .sector_cnt     = (MB(1) / 4096),
-    .flash_start    = 0,
-    .flash_end      = MB(1),
-    .ram_start      = 0x1FFF0000,
-    .ram_end        = 0x20030000,
-    .flash_algo     = (program_target_t *) &flash,
+    .flash_start        = 0x00000000,
+    .flash_end          = 0x00100000,
+    .ram_start          = 0x1fff0000,
+    .ram_end            = 0x20030000,
+    .flash_algo         = (program_target_t *) &flash,
+    .sectors_info       = sectors_info,
+    .sector_info_length = (sizeof(sectors_info))/(sizeof(sector_info_t))
 };
