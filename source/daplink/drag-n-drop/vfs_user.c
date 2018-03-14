d96959b657bab83bc61d8866edcd8c2dce2af4f2
Arek Zaluski <Arkadiusz.Zaluski@arm.com>
2018-03-14 05:12:39 +0000

    Add WebUSB functionality
    * Add Microsoft OS 2.0 Descriptor support
    
    * Add WebUSB interface
    
    * Set landing page to os.mbed.com
    
    * Flashing a board
    
    * Reading/Writing serial data

--- source/daplink/drag-n-drop/vfs_user.c
+++ source/daplink/drag-n-drop/vfs_user.c
@@ -292,6 +292,9 @@ static uint32_t read_file_details_txt(uint32_t sector_offset, uint8_t *data, uin
 #ifdef HID_ENDPOINT
     pos += util_write_string(buf + pos, ", HID");
 #endif
+#if (WEBUSB_INTERFACE)
+    pos += util_write_string(buf + pos, ", WebUSB");
+#endif
     pos += util_write_string(buf + pos, "\r\n");
 
     // CRC of the bootloader (if there is one)
