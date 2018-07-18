c072cf516b32860bf8df9ad367b3c01d84429e29
dragoniteArm <39923906+dragoniteArm@users.noreply.github.com>
2018-07-18 15:47:10 -0500

    change back to software reset

--- source/target/ti/target_reset.c
+++ source/target/ti/target_reset.c
@@ -34,7 +34,7 @@ uint8_t target_unlock_sequence(void)
 
 uint8_t target_set_state(TARGET_RESET_STATE state)
 {
-        return swd_set_target_state_hw(state);
+        return swd_set_target_state_sw(state);
 }
 
 uint8_t security_bits_set(uint32_t addr, uint8_t *data, uint32_t size)
