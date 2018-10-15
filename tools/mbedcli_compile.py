93233ae87b68f552dcb37476544d577a88603d59
Brian Esquilona <brian.esquilona@arm.com>
2018-10-15 17:56:53 -0500

    Parse project list from projects.yaml, parse version from version.yaml

--- tools/mbedcli_compile.py
+++ tools/mbedcli_compile.py
@@ -20,12 +20,16 @@ import argparse
 import os
 import shutil
 import sys
+import yaml
 
 from subprocess import check_output, CalledProcessError
 from pre_build_script import generate_version_file
 from generate_mbedcli_files import generate_mbedcli_files
 from post_compute_crc import post_compute_crc
 from package_release_files import package_release_files
+
+PROJECTS_YAML = "projects.yaml"
+VERSION_YAML = "version.yaml"
 
 def mbedcli_compile(daplink_dir, build_folder, project, toolchain, clean):
     generate_mbedcli_files(os.path.join(daplink_dir, "projects.yaml"), project)
@@ -48,52 +52,71 @@ def mbedcli_compile(daplink_dir, build_folder, project, toolchain, clean):
         exit(1)
 
 def main():
-    """python -u tools/mbedcli_compile.py --project kl26z_microbit_if kl26z_bl --clean"""
-    parser = argparse.ArgumentParser(description='mbedcli compile support for DAPLink')
-    parser.add_argument('--project', help='Selectively compile only the firmware specified',
-                        nargs='+', type=str, default=[], required=False)
-    parser.add_argument('--release_version', type=int, help='Create a release with this version')
-    parser.add_argument('--build_folder', type=str, default='BUILD', help='Release directory to grab files from')
-    parser.add_argument('--release_folder', type=str, default='firmware', help='Directory to create and place files in')
+    """python -u tools/mbedcli_compile.py kl26z_microbit_if kl26z_bl --clean"""
+
+    #parse project yaml file for project list
+    project_list = []
+    with open(PROJECTS_YAML, 'r') as top_yaml:
+        try:
+            topdict = yaml.load(top_yaml)
+            for dict_key in topdict:
+                if dict_key == 'projects':
+                    for project in topdict[dict_key]:
+                        project_list.append(project)
+                    break
+        except yaml.YAMLError as ex:
+            print("Found yaml parse error", ex)
+
+    #parse the arguments
+    projects = "List of supported projects\n\n" + "\n".join(project_list) #
+    parser = argparse.ArgumentParser(description='mbedcli compile support for DAPLink', epilog=projects, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('projects', help='Selectively compile only the firmware specified',
+                        nargs='*', type=str, default=[])
+    parser.add_argument('--release', dest='release', action='store_true', help='Create a release with the yaml version file')
+    parser.add_argument('--build-folder', type=str, default='BUILD', help='Release directory to grab files from')
+    parser.add_argument('--release-folder', type=str, default='firmware', help='Directory to create and place files in')
     parser.add_argument('--toolchain', type=str, default='ARM', help='Toolchain directory if present')
     parser.add_argument('--clean', dest='clean', action='store_true', help='Rebuild or delete build folder before compile')
     parser.set_defaults(clean=False)
+    parser.set_defaults(release=False)
     args = parser.parse_args()
+
+
     self_path = os.path.abspath(__file__)
     tools_dir = os.path.dirname(self_path)
     daplink_dir = os.path.dirname(tools_dir)
-    test_dir = os.path.join(daplink_dir, "test")
-    sys.path.append(test_dir)
-    #get all projects
-    import info
     if os.path.basename(tools_dir) != "tools":
         print("Error - this script must be run from the tools directory")
         exit(-1)
     version_git_dir = os.path.join(daplink_dir, "source", "daplink")
     generate_version_file(version_git_dir)
-    if not args.project == []: 
+    if not args.projects == []: 
 
-        for project in args.project:
+        for project in args.projects:
+            print("Compiling %s" % project)
             mbedcli_compile(daplink_dir, args.build_folder, project, args.toolchain, args.clean)
     else:
         print("compiling all firmware")
-        bootloader_list = []
-        #compile all the bootloader
-        for board_id, fimware, bootloader, target in info.SUPPORTED_CONFIGURATIONS:
-            if bootloader is not None and bootloader not in bootloader_list:
-                bootloader_list.append(bootloader)
-                mbedcli_compile(daplink_dir, args.build_folder, bootloader, args.toolchain, True)
 
-        #compile all projects in the release info
-        for prj_name, legacy, offset, extension in info.PROJECT_RELEASE_INFO:
-            print("Compiling %s" % prj_name)
-            mbedcli_compile(daplink_dir, args.build_folder, prj_name, args.toolchain, True)
+        for project in project_list:
+            print("Compiling %s" % project)
+            mbedcli_compile(daplink_dir, args.build_folder, project, args.toolchain, args.clean)
 
-        if args.release_version is not None:
-            release_dir = args.release_folder + "%04i" % args.release_version 
-            if os.path.exists(release_dir):
-                print("Deleting %s" % release_dir)
-                shutil.rmtree(release_dir, ignore_errors=True)
-            package_release_files(args.build_folder, release_dir, args.release_version, args.toolchain)
+    if args.release is True:
+        release_version = 0
+        with open(os.path.join("records","tools", VERSION_YAML), 'r') as ver_yaml:
+            try:
+                verdict = yaml.load(ver_yaml)
+                release_version = int(verdict['common']['macros'][0].split('=')[1])
+            except yaml.YAMLError as ex:
+                print("Found yaml parse error", ex)
+
+        release_dir = args.release_folder + "_%04i" % release_version 
+        if os.path.exists(release_dir):
+            print("Deleting %s" % release_dir)
+            shutil.rmtree(release_dir, ignore_errors=True)
+        print("Releasing directory: " + release_dir)
+        toolchain_dir = args.toolchain+"-CUSTOM_PROFILE"
+        package_release_files(args.build_folder, release_dir, release_version, toolchain_dir)
 
 main()
