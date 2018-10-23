937f47218b232b6553c3fb9c0905001745a91935
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2018-10-23 13:17:29 -0500

    Add support for LPC54018 IoT module board
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

---
+++ source/board/mini_iot_lpc54018.c
@@ -0,0 +1,41 @@
+/**
+ * @file    iotmodule_lpc54018.c
+ * @brief   board ID for the NXP LPC54608Xpresso board
+ *
+ * DAPLink Interface Firmware
+ * Copyright (c) 2018, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "virtual_fs.h"
+#include "flash_manager.h"
+
+const char *board_id = "0235";
+
+// Override default behavior
+//
+// Note - 4 byte alignemnt required as workaround for ARMCC compiler bug with weak references
+__attribute__((aligned(4)))
+const vfs_filename_t daplink_url_name =       "PRODINFOHTM";
+__attribute__((aligned(4)))
+const vfs_filename_t daplink_drive_name =     "MINI-54018";
+__attribute__((aligned(4)))
+const char *const daplink_target_url = "https://os.mbed.com/platforms/LPC54018IoTModule/";
+
+void prerun_board_config(void)
+{
+    flash_manager_set_page_erase(true);
+}
+
