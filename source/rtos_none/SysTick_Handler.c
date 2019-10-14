593ea2874df9490587e54bf6f0c49403d7065dc9
ychsu <ychsu.tf@gmail.com>
2019-10-14 15:05:32 +0800

    Add Nuvoton M48SSIDAE HIC and NuMaker-M252KG board

--- source/rtos_none/SysTick_Handler.c
+++ source/rtos_none/SysTick_Handler.c
@@ -39,6 +39,8 @@
 #include "sam3u2c.h"
 #elif defined (INTERFACE_STM32F103XB)
 #include "stm32f103xb.h"
+#elif defined (INTERFACE_M48SSIDAE)
+#include "M480.h"
 #else
 #error "CMSIS core headers needed"
 #endif
