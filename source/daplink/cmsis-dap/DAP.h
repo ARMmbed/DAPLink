5aaa46494d4c4516554a4355f3274724c8fdf2da
Chris Reed <flit@me.com>
2020-12-20 16:02:06 -0600

    CMSIS-DAP: prepare for custom SWD implementation.
    - Saving nominal requested clock frequency in DAP_Data.
    - Made SWD bit banging functions weak.

--- source/daplink/cmsis-dap/DAP.h
+++ source/daplink/cmsis-dap/DAP.h
@@ -211,6 +211,7 @@ typedef struct {
   uint8_t     fast_clock;                       // Fast Clock Flag
   uint8_t     padding[2];
   uint32_t   clock_delay;                       // Clock Delay
+  uint32_t nominal_clock;                       // Nominal requested clock frequency in Hertz.
   uint32_t     timestamp;                       // Last captured Timestamp
   struct {                                      // Transfer Configuration
     uint8_t   idle_cycles;                      // Idle cycles after transfer
