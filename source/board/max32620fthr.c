2b5fda8bf81289f7609cae35129cf8c9ff897047
Keyur Hariya <Keyur.Hariya@maximintegrated.com>
2021-03-19 12:32:45 -0500

    Add custom reset for MAX32620 targets

--- source/board/max32620fthr.c
+++ source/board/max32620fthr.c
@@ -25,7 +25,7 @@
 const board_info_t g_board_info = {
     .info_version = kBoardInfoVersion,
     .board_id = "0418",
-    .family_id = kStub_HWReset_FamilyID,
+    .family_id = kMaxim_MAX32620_FamilyID,
     .flags = kEnablePageErase,
     .target_cfg = &target_device,
 };
