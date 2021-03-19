43538f708431067d605acf94a29c040a9ceea445
Keyur Hariya <Keyur.Hariya@maximintegrated.com>
2021-03-19 12:32:45 -0500

    Add custom reset for MAX32620 targets

--- source/target/target_family.c
+++ source/target/target_family.c
@@ -74,6 +74,8 @@ __attribute__((weak))
 const target_family_descriptor_t g_maxim_max32660  = {0};
 __attribute__((weak))
 const target_family_descriptor_t g_maxim_max32630  = {0};
+__attribute__((weak))
+const target_family_descriptor_t g_maxim_max32620  = {0};
 
 __attribute__((weak))
 const target_family_descriptor_t *g_families[] = {
@@ -95,6 +97,7 @@ const target_family_descriptor_t *g_families[] = {
     &g_ambiq_ama3b1kk,
     &g_maxim_max32660,
     &g_maxim_max32630,
+    &g_maxim_max32620,
     0 // list terminator
 };
 
