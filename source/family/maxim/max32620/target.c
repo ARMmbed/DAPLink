6aa92f70a3ecdb173cdaa0810e29194545c133eb
Keyur Hariya <Keyur.Hariya@maximintegrated.com>
2021-03-19 14:06:40 -0500

    Cleanup

--- source/family/maxim/max32620/target.c
+++ source/family/maxim/max32620/target.c
@@ -23,14 +23,14 @@
 
 #include "flash_blob.c"
 
-/* ME02 -- MAX32620 2MiB Flash, 256KiB RAM */
+/* MAX32620 2MiB Flash, 256KiB RAM */
 target_cfg_t target_device = {
     .sectors_info                   = sectors_info,
     .sector_info_length             = (sizeof(sectors_info))/(sizeof(sector_info_t)),
-    .flash_regions[0].start         = 0,
-    .flash_regions[0].end           = 0x200000,
+    .flash_regions[0].start         = FLASH_BASE,
+    .flash_regions[0].end           = FLASH_BASE + FLASH_SIZE,
     .flash_regions[0].flags         = kRegionIsDefault,
-    .flash_regions[0].flash_algo    = (program_target_t *) &flash,    
+    .flash_regions[0].flash_algo    = (program_target_t *) &flash,
     .ram_regions[0].start           = 0x20000000,
-    .ram_regions[0].end             = 0x20040000,
+    .ram_regions[0].end             = 0x20000000 + KB(256),
 };
