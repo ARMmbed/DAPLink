5644871c4412c3a23b329ca08e5125c400c17200
Syam Mohan V S <sm.ts.biz@gmail.com>
2021-04-03 00:20:32 +0530

    Update main_interface.c

--- source/daplink/interface/main_interface.c
+++ source/daplink/interface/main_interface.c
@@ -246,7 +246,7 @@ void main_task(void * arg)
 	// leds
     gpio_init();
     // Turn to LED default settings
-	HAL_GPIO_WritePin(RUNNING_LED_PORT , RUNNING_LED_PIN, GPIO_RESET );
+	HAL_GPIO_WritePin(RUNNING_LED_PORT , RUNNING_LED_PIN, GPIO_PIN_RESET);
  //   gpio_set_hid_led(hid_led_value);
     while(1);
    	gpio_set_cdc_led(cdc_led_value);
