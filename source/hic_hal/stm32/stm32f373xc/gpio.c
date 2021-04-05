e8737fbc25196c8436bb0e64d9b4facc4ec76906
symovs <sm.ts.biz@gmail.com>
2021-04-05 14:15:56 +0530

    Update gpio.c

--- source/hic_hal/stm32/stm32f373xc/gpio.c
+++ source/hic_hal/stm32/stm32f373xc/gpio.c
@@ -204,7 +204,8 @@ void gpio_init(void)
         //HAL_Delay(10000);
 		//gpio_set_hid_led(GPIO_LED_ON);
 		//HAL_Delay(10000);
-		HAL_GPIO_WritePin(PIN_HID_LED_PORT, PIN_HID_LED, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(RUNNING_LED_PORT, RUNNING_LED_PIN, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(PIN_HID_LED_PORT, PIN_HID_LED, GPIO_PIN_RESET);
     }
 	
 }
