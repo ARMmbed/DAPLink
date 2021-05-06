7b60a5a96d03809407b0aae5a7b13757d7de05e6
Taylor Heck <taylor.heck@multitech.com>
2021-05-06 16:10:33 -0500

    Fix XDot RESET_PROGRAM.

--- source/board/xDot-L151.c
+++ source/board/xDot-L151.c
@@ -21,12 +21,62 @@
 
 #include "target_family.h"
 #include "target_board.h"
+#define DBG_Addr     (0xe000edf0)
+#include "swd_host.h"
+#include "cmsis_os2.h"
+#include "debug_cm.h"
+
+static uint8_t xdot_set_state(target_state_t state)
+{
+    uint32_t val;
+    
+    if (state == RESET_PROGRAM) {
+        swd_init();
+        swd_set_target_reset(1);
+        osDelay(2);
+
+        if (!swd_init_debug()) {
+            return 0;
+        }
+
+        // Enable debug
+        if (!swd_write_word(DBG_HCSR, DBGKEY | C_HALT | C_DEBUGEN)) {
+            return 0;
+        }
+
+        // Enable halt on reset
+        if (!swd_write_word(DBG_EMCR, VC_CORERESET)) {
+            return 0;
+        }
+
+        // Deassert reset
+        swd_set_target_reset(0);
+        osDelay(2);
+
+        do {
+            if (!swd_read_word(DBG_HCSR, &val)) {
+                return 0;
+            }
+        } while ((val & S_HALT) == 0);
+
+        // Disable halt on reset
+        if (!swd_write_word(DBG_EMCR, 0)) {
+            return 0;
+        }
+    } else {
+        return swd_set_target_state_hw(state);
+    }
+
+    return 1;
+}
 
 const board_info_t g_board_info = {
     .info_version = kBoardInfoVersion,
     .board_id = "0350",
     .family_id = kStub_HWReset_FamilyID,
-    .daplink_url_name =       "XDOT    HTM",
-    .daplink_drive_name =       "XDOT       ",
+    .flags = kEnableUnderResetConnect,
+    .daplink_url_name = "XDOT    HTM",
+    .daplink_drive_name =  "XDOT       ",
     .target_cfg = &target_device,
+    .target_set_state = xdot_set_state
 };
