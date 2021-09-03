7fc7b1d87f14633dc8cae51061232991caffcd8f
Sadik.Ozer <Sadik.Ozer@maximintegrated.com>
2021-09-03 14:16:41 +0300

    Add custom reset for MAX32660
    Signed-off-by: Sadik.Ozer <Sadik.Ozer@maximintegrated.com>

--- source/target/target_family.c
+++ source/target/target_family.c
@@ -70,7 +70,8 @@ __attribute__((weak))
 const target_family_descriptor_t g_toshiba_tz_family  = {0};
 __attribute__((weak))
 const target_family_descriptor_t g_ambiq_ama3b1kk  = {0};
-
+__attribute__((weak))
+const target_family_descriptor_t g_maxim_max32660 = {0};
 
 __attribute__((weak))
 const target_family_descriptor_t *g_families[] = {
@@ -90,6 +91,7 @@ const target_family_descriptor_t *g_families[] = {
     &g_renesas_family,
     &g_toshiba_tz_family,
     &g_ambiq_ama3b1kk,
+    &g_maxim_max32660,
     0 // list terminator
 };
 
