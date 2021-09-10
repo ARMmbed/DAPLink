dc4a5a3944ac5af2206b5484a1cef7485ee1ed03
Mathias Brossard <mathias.brossard@arm.com>
2021-09-09 21:25:11 -0500

    nrf52820_hic: bump default SWD frequency to 8 MHz

--- source/hic_hal/nordic/nrf52820/DAP_config.h
+++ source/hic_hal/nordic/nrf52820/DAP_config.h
@@ -83,7 +83,7 @@ This information includes:
 /// Default communication speed on the Debug Access Port for SWD and JTAG mode.
 /// Used to initialize the default SWD/JTAG clock frequency.
 /// The command \ref DAP_SWJ_Clock can be used to overwrite this default setting.
-#define DAP_DEFAULT_SWJ_CLOCK   1000000U        ///< Default SWD/JTAG clock frequency in Hz.
+#define DAP_DEFAULT_SWJ_CLOCK   8000000U        ///< Default SWD/JTAG clock frequency in Hz.
 
 /// Maximum Package Size for Command and Response data.
 /// This configuration settings is used to optimize the communication performance with the
