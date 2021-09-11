e01415b73b4580798bc22bad556dc75a22df21af
Mathias Brossard <mathias@brossard.org>
2021-09-10 23:01:29 -0500

    Fix configuration for boards that use 64 KB version of nRF52832

--- source/board/96b_nitrogen.c
+++ source/board/96b_nitrogen.c
@@ -22,11 +22,11 @@
 #include "target_board.h"
 #include "target_family.h"
 
-extern target_cfg_t target_device_nrf52;
+extern target_cfg_t target_device_nrf52_64;
 
 const board_info_t g_board_info = {
     .info_version = kBoardInfoVersion,
     .board_id = "9014",
     .family_id = kNordic_Nrf52_FamilyID,
-    .target_cfg = &target_device_nrf52,
+    .target_cfg = &target_device_nrf52_64,
 };
