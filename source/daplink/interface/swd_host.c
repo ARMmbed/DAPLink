4021607dca616069648db4e09a0f7702f5d07788
Mathias Brossard <mathias.brossard@arm.com>
2021-10-15 19:16:48 -0500

    Fix MSD flashing for kl26z (microbit) (#853)
    * Call swd_init_debug() after reset + fix ACK checks

--- source/daplink/interface/swd_host.c
+++ source/daplink/interface/swd_host.c
@@ -4,7 +4,7 @@
  *
  * DAPLink Interface Firmware
  * Copyright (c) 2009-2019, ARM Limited, All Rights Reserved
- * Copyright 2019, Cypress Semiconductor Corporation 
+ * Copyright 2019, Cypress Semiconductor Corporation
  * or a subsidiary of Cypress Semiconductor Corporation.
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -1051,35 +1051,39 @@ uint8_t swd_set_target_state_sw(target_state_t state)
             osDelay(2);
             swd_set_target_reset(0);
             osDelay(2);
-        
+
+            if (!swd_init_debug()) {
+                return 0;
+            }
+
             // Power down
             // Per ADIv6 spec. Clear first CSYSPWRUPREQ followed by CDBGPWRUPREQ
             if (!swd_read_dp(DP_CTRL_STAT, &val)) {
                 return 0;
             }
-            
+
             if (!swd_write_dp(DP_CTRL_STAT, val & ~CSYSPWRUPREQ)) {
                 return 0;
             }
-            
+
             // Wait until ACK is deasserted
             do {
                 if (!swd_read_dp(DP_CTRL_STAT, &val)) {
                     return 0;
                 }
-            } while ((val & (CSYSPWRUPACK)) == 1);
-            
+            } while ((val & (CSYSPWRUPACK)) != 0);
+
             if (!swd_write_dp(DP_CTRL_STAT, val & ~CDBGPWRUPREQ)) {
                 return 0;
             }
-            
+
             // Wait until ACK is deasserted
             do {
                 if (!swd_read_dp(DP_CTRL_STAT, &val)) {
                     return 0;
                 }
-            } while ((val & (CDBGPWRUPACK)) == 1);
-        
+            } while ((val & (CDBGPWRUPACK)) != 0);
+
             swd_off();
             break;
 
