172ab04f1bf13941965899609eb02ffaa9ba0c59
Mathias Brossard <mathias@brossard.org>
2021-11-04 00:18:41 -0500

    lpc55xx_hic: add support for MCU-LINK-PRO and nRESET
    - Support for nRESET as reset button
    - Detect MCU-LINK-PRO  and support additional LEDs

--- source/hic_hal/nxp/lpc55xx/gpio.c
+++ source/hic_hal/nxp/lpc55xx/gpio.c
@@ -1,6 +1,6 @@
 /**
  * @file    gpio.c
- * @brief
+ * @brief   GPIO handling for LPC55xx HIC, MCU-LINK and MCU-LINK-PRO
  *
  * DAPLink Interface Firmware
  * Copyright (c) 2009-2016, ARM Limited, All Rights Reserved
@@ -29,6 +29,8 @@
 #include "fsl_iocon.h"
 #include "fsl_reset.h"
 
+static bool mcu_link_pro = false;
+
 void gpio_init(void)
 {
     // Enable hardfault on unaligned access for the interface only.
@@ -40,28 +42,67 @@ void gpio_init(void)
 
     // Ensure clocks are enabled.
     SYSCON->AHBCLKCTRLSET[0] = SYSCON_AHBCLKCTRL0_IOCON_MASK
-                                | SYSCON_AHBCLKCTRL0_GPIO0_MASK;
+                                | SYSCON_AHBCLKCTRL0_GPIO0_MASK
+                                | SYSCON_AHBCLKCTRL0_GPIO1_MASK;
     SYSCON->AHBCLKCTRLSET[1] = SYSCON_AHBCLKCTRL1_FC0_MASK
                                 | SYSCON_AHBCLKCTRL1_FC3_MASK;
 
     // Reset peripherals.
     RESET_PeripheralReset(kIOCON_RST_SHIFT_RSTn);
     RESET_PeripheralReset(kGPIO0_RST_SHIFT_RSTn);
+    RESET_PeripheralReset(kGPIO1_RST_SHIFT_RSTn);
     RESET_PeripheralReset(kFC0_RST_SHIFT_RSTn);
     RESET_PeripheralReset(kFC3_RST_SHIFT_RSTn);
 
+    // Configure pins for board detection
+    IOCON->PIO[PIN_HW_VERS_3_PORT][PIN_HW_VERS_3] = IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGITAL_EN | IOCON_OPENDRAIN_EN;
+    IOCON->PIO[PIN_HW_VERS_4_PORT][PIN_HW_VERS_4] = IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGITAL_EN | IOCON_OPENDRAIN_EN;
+    GPIO->DIRCLR[PIN_HW_VERS_3_PORT] = PIN_HW_VERS_3_MASK;
+    GPIO->DIRCLR[PIN_HW_VERS_4_PORT] = PIN_HW_VERS_4_MASK;
+    if (GPIO->B[PIN_HW_VERS_4_PORT][PIN_HW_VERS_4] == 0 && GPIO->B[PIN_HW_VERS_3_PORT][PIN_HW_VERS_3] != 0) {
+        mcu_link_pro = true;
+    }
+
     // Configure pins.
-    IOCON->PIO[PIN_PIO_PORT][LED_CONNECTED] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+    IOCON->PIO[LED_A_PORT][LED_A_PIN] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+    IOCON->PIO[PIN_PIO_PORT][PIN_RESET] = IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGITAL_EN | IOCON_OPENDRAIN_EN;
     IOCON->PIO[PIN_PIO_PORT][PIN_HW_VERS_6] = IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGITAL_EN | IOCON_OPENDRAIN_EN;
     IOCON->PIO[PIN_PIO_PORT][PIN_HW_VERS_7] = IOCON_FUNC0 | IOCON_MODE_PULLUP | IOCON_DIGITAL_EN | IOCON_OPENDRAIN_EN;
+    if (mcu_link_pro) {
+        IOCON->PIO[LED_B_PORT][LED_B_PIN] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+        IOCON->PIO[LED_C_PORT][LED_C_PIN] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+        IOCON->PIO[LED_D_PORT][LED_D_PIN] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+        IOCON->PIO[LED_E_PORT][LED_E_PIN] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+        IOCON->PIO[LED_F_PORT][LED_F_PIN] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+        IOCON->PIO[LED_G_PORT][LED_G_PIN] = IOCON_FUNC0 | IOCON_DIGITAL_EN;
+    }
 
     // Turn off LED.
-    GPIO->B[PIN_PIO_PORT][LED_CONNECTED] = 1;
-    // Set LED to output.
-    GPIO->DIRSET[PIN_PIO_PORT] = LED_CONNECTED_MASK;
-    // Turn on LED.
-    GPIO->B[PIN_PIO_PORT][LED_CONNECTED] = 1;
+    GPIO->B[LED_A_PORT][LED_A_PIN] = 1;
+    if (mcu_link_pro) {
+        GPIO->B[LED_B_PORT][LED_B_PIN] = 1;
+        GPIO->B[LED_C_PORT][LED_C_PIN] = 1;
+        GPIO->B[LED_D_PORT][LED_D_PIN] = 1;
+        GPIO->B[LED_E_PORT][LED_E_PIN] = 1;
+        GPIO->B[LED_F_PORT][LED_F_PIN] = 1;
+        GPIO->B[LED_G_PORT][LED_G_PIN] = 1;
+    }
 
+    // Set LED to output.
+    GPIO->DIRSET[LED_A_PORT] = LED_A_MASK;
+    if (mcu_link_pro) {
+        GPIO->DIRSET[LED_B_PORT] = LED_B_MASK;
+        GPIO->DIRSET[LED_C_PORT] = LED_C_MASK;
+        GPIO->DIRSET[LED_D_PORT] = LED_D_MASK;
+        GPIO->DIRSET[LED_E_PORT] = LED_E_MASK;
+        GPIO->DIRSET[LED_F_PORT] = LED_F_MASK;
+        GPIO->DIRSET[LED_G_PORT] = LED_G_MASK;
+    }
+
+    // Turn on LED.
+    GPIO->B[LED_A_PORT][LED_A_PIN] = 0;
+
+    GPIO->DIRCLR[PIN_PIO_PORT] = PIN_RESET_MASK;
     GPIO->DIRCLR[PIN_PIO_PORT] = PIN_HW_VERS_6_MASK;
     GPIO->DIRCLR[PIN_PIO_PORT] = PIN_HW_VERS_7_MASK;
 }
@@ -71,25 +112,54 @@ void gpio_set_board_power(bool powerEnabled)
     // No target power control in this circuit.
 }
 
-void gpio_set_hid_led(gpio_led_state_t state)
+void gpio_set_leds(uint32_t leds, gpio_led_state_t state)
 {
     // LED is active low, so set to inverse of the enum value.
-    GPIO->B[PIN_PIO_PORT][LED_CONNECTED] = !(uint8_t)state;
+    if(mcu_link_pro) {
+        if(leds & LED_T_CONNECTED) {
+            GPIO->B[LED_A_PORT][LED_A_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+        }
+        if(leds & LED_T_RUNNING) {
+            GPIO->B[LED_B_PORT][LED_B_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+        }
+        if(leds & LED_T_HID) {
+            GPIO->B[LED_C_PORT][LED_C_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+        }
+        if(leds & LED_T_CDC) {
+            GPIO->B[LED_D_PORT][LED_D_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+        }
+        if(leds & LED_T_MSC) {
+            GPIO->B[LED_E_PORT][LED_E_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+        }
+        if(leds & LED_T_EXTRA1) {
+            GPIO->B[LED_F_PORT][LED_F_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+        }
+        if(leds & LED_T_EXTRA2) {
+            GPIO->B[LED_G_PORT][LED_G_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+        }
+    } else if(leds & (LED_T_CONNECTED | LED_T_HID | LED_T_CDC | LED_T_MSC)) {
+        GPIO->B[LED_A_PORT][LED_A_PIN] = (uint8_t)((state == GPIO_LED_ON) ? 0 : 1);
+    }
+}
+
+void gpio_set_hid_led(gpio_led_state_t state)
+{
+    gpio_set_leds(LED_T_HID, state);
 }
 
 void gpio_set_cdc_led(gpio_led_state_t state)
 {
-    gpio_set_hid_led(state);
+    gpio_set_leds(LED_T_CDC, state);
 }
 
 void gpio_set_msc_led(gpio_led_state_t state)
 {
-    gpio_set_hid_led(state);
+    gpio_set_leds(LED_T_MSC, state);
 }
 
 uint8_t gpio_get_reset_btn_no_fwrd(void)
 {
-    return GPIO->B[PIN_PIO_PORT][PIN_HW_VERS_6] ? 0 : 1;
+    return (PIN_nRESET_IN() && GPIO->B[PIN_PIO_PORT][PIN_HW_VERS_6]) ? 0 : 1;
 }
 
 uint8_t gpio_get_reset_btn_fwrd(void)
