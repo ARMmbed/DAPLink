8df64c34d626b86256117a66f4435c4a3abd648f
Mathias Brossard <mathias@brossard.org>
2021-11-23 09:53:39 -0600

    debug: add RTT output support (#899)

--- source/daplink/daplink_debug.h
+++ source/daplink/daplink_debug.h
@@ -3,7 +3,7 @@
  * @brief   optional trace messages useful in development
  *
  * DAPLink Interface Firmware
- * Copyright (c) 2009-2016, ARM Limited, All Rights Reserved
+ * Copyright (c) 2009-2021, Arm Limited, All Rights Reserved
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -26,65 +26,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
-#include "cmsis_os2.h"
-#include "rl_usb.h"
-#include "util.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#ifndef MSC_DEBUG
-//#define MSC_DEBUG
+#ifndef DAPLINK_DEBUG
+// #define DAPLINK_DEBUG
 #endif
 
-#if defined (MSC_DEBUG)
+#if defined (DAPLINK_DEBUG)
 
-static const char error_msg[] = "\r\n<OVERFLOW>\r\n";
-
-static inline uint32_t daplink_debug(uint8_t *buf, uint32_t size)
-{
-    uint32_t total_free;
-    uint32_t write_free;
-    uint32_t error_len = strlen(error_msg);
-    total_free = USBD_CDC_ACM_DataFree();
-
-    if (total_free < error_len) {
-        // No space
-        return 0;
-    }
-
-    // Size available for writing
-    write_free = total_free - error_len;
-    size = MIN(write_free, size);
-    USBD_CDC_ACM_DataSend(buf, size);
-
-    if (write_free == size) {
-        USBD_CDC_ACM_DataSend((uint8_t *)error_msg, error_len);
-    }
-
-    return size;
-}
-
-static char daplink_debug_buf[128] = {0};
-static inline uint32_t daplink_debug_print(const char *format, ...)
-{
-    uint32_t ret;
-    int32_t r = 0;
-    va_list arg;
-    ret = 1;
-    va_start(arg, format);
-    r = vsnprintf(daplink_debug_buf, sizeof(daplink_debug_buf), format, arg);
-
-    if (r >= sizeof(daplink_debug_buf)) {
-        r = snprintf(daplink_debug_buf, sizeof(daplink_debug_buf), "<Error - string length %i exceeds print buffer>\r\n", r);
-        ret = 0;
-    }
-
-    va_end(arg);
-    daplink_debug((uint8_t *)daplink_debug_buf, r);
-    return ret;
-}
+uint32_t daplink_debug_print(const char *format, ...);
+uint32_t daplink_debug(uint8_t *data, uint32_t size);
 
 #else
 
@@ -103,8 +57,8 @@ static inline uint32_t daplink_debug(uint8_t *data, uint32_t size)
 #define debug_msg(fmt, args...) daplink_debug_print(fmt, ## args);
 #define debug_data(buf, size) daplink_debug(buf, size);
 
-#ifdef __cplusplus
-}
 #endif
 
+#ifdef __cplusplus
+}
 #endif
