43e7e42854872d067be4f8e42d763b3209fa4067
Mathias Brossard <mathias@brossard.org>
2021-12-05 01:11:38 -0600

    Fixes for multiple MIMXRT boards

--- source/family/freescale/mimxrt1050_hyper_flash/target.c
+++ source/family/freescale/mimxrt1050_hyper_flash/target.c
@@ -34,7 +34,7 @@ target_cfg_t target_device = {
     .flash_regions[0].flags         = kRegionIsDefault,
     .flash_regions[0].flash_algo    = (program_target_t *) &flash,
     .ram_regions[0].start           = 0x20000000,
-    .ram_regions[0].end             = 0x20000000 + MB(64),
+    .ram_regions[0].end             = 0x20000000 + KB(512),
     .target_vendor                  = "NXP",
     .target_part_number             = "MIMXRT1052DVL6A",
 };
