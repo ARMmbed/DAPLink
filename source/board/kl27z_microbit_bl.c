be2b4f7563388dc0e22edb0e80acc36ce3fc1ba8
Carlos Pereira Atencio <carlos@microbit.org>
2022-01-12 05:56:50 +0000

    microbitv2_bl: Add non-zero board ID to micro:bit DAPLink bootloader (#928)

--- source/board/kl27z_microbit_bl.c
+++ source/board/kl27z_microbit_bl.c
@@ -59,7 +59,7 @@ const target_family_descriptor_t *g_target_family = NULL;
 
 const board_info_t g_board_info = {
     .info_version = kBoardInfoVersion,
-    .board_id = "0000",
+    .board_id = "9903",
     .daplink_url_name =       "HELP_FAQHTM",
     .daplink_drive_name =       "MAINTENANCE",
     .daplink_target_url = "https://microbit.org/device/?id=@B&v=@V&bl=1",
