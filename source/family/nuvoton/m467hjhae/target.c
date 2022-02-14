4babfb13e2b9854ab7468a55e5bc84842a1c355b
ychsu <ychsu.tf@gmail.com>
2022-02-14 11:46:58 +0800

    Add Nuvoton NuMaker-IoT-M467 board

---
+++ source/family/nuvoton/m467hjhae/target.c
@@ -0,0 +1,43 @@
+/**
+ * @file    target.c
+ * @brief   Target information for the m467hjhae
+ *
+ * DAPLink Interface Firmware
+ * Copyright (c) 2022 Nuvoton Technology Corp. All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "target_config.h"
+
+// The file flash_blob.c must only be included in target.c
+#include "flash_blob.c"
+
+// target information
+target_cfg_t target_device = {
+    .version                        = kTargetConfigVersion,
+    .sectors_info                   = sectors_info,
+    .sector_info_length             = (sizeof(sectors_info))/(sizeof(sector_info_t)),
+    .flash_regions[0].start         = 0x00000000,
+    .flash_regions[0].end           = 0x00100000,
+    .flash_regions[0].flags         = kRegionIsDefault,
+    .flash_regions[0].flash_algo    = (program_target_t *)&flash,
+    .flash_regions[1].start         = 0x0F100000,
+    .flash_regions[1].end           = 0x0F102000,
+    .flash_regions[1].flash_algo    = (program_target_t *)&flash,
+    .ram_regions[0].start           = 0x20000000,
+    .ram_regions[0].end             = 0x20080000,
+    .target_vendor                  = "Nuvoton",
+    .target_part_number             = "M467HJHAE",
+};
